986512a49a111f0cfc38d70c9f61f1476463a48e
Piotr Buda <pibuda@gmail.com>
2013-03-22 09:41:08 +0100

    Update authentication and remember user signed in with GitHub
    Update existing user with new credentials or create a new user. Enable remembering of user who signed in with GitHub.

--- codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/GithubAuthorizationServlet.scala
+++ codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/GithubAuthorizationServlet.scala
@@ -4,6 +4,8 @@ import com.softwaremill.codebrag.service.github.GitHubAuthService
 import org.scalatra.SeeOther
 import com.softwaremill.codebrag.dao.UserDAO
 import com.softwaremill.codebrag.service.user.Authenticator
+import com.softwaremill.codebrag.domain.{User, Authentication}
+import java.util.UUID
 
 
 class GithubAuthorizationServlet(val authenticator:Authenticator, ghAuthService:GitHubAuthService, userDao:UserDAO) extends JsonServletWithAuthentication {
@@ -15,9 +17,10 @@ class GithubAuthorizationServlet(val authenticator:Authenticator, ghAuthService:
     val code = params.get("code").get
     val accessToken = ghAuthService.getAccessToken(code)
     val user = ghAuthService.loadUserData(accessToken)
+    val auth: Authentication = Authentication.github(user.login, accessToken.access_token)
     userDao.findByEmail(user.email) match {
-      case Some(u) =>
-      case None =>
+      case Some(u) => userDao.changeAuthentication(u.id, auth)
+      case None => userDao.add(User(auth, user.name, user.email, UUID.randomUUID().toString))
     }
     tmpLogin = user.login
     tmpPassword = accessToken.access_token
@@ -28,6 +31,8 @@ class GithubAuthorizationServlet(val authenticator:Authenticator, ghAuthService:
   override protected def login: String = tmpLogin
 
   override protected def password: String = tmpPassword
+
+  override protected def rememberMe: Boolean = true
 }
 
 
