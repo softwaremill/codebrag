1cc9a2e707ebe8d2c251969dfc238e9fde8aa930
Piotr Buda <pibuda@gmail.com>
2013-03-26 14:09:12 +0100

    Add service to handle diffs

---
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/diff/DiffService.scala
@@ -0,0 +1,5 @@
+package com.softwaremill.codebrag.service.diff
+
+class DiffService {
+
+}
---
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/diff/DiffServiceSpec.scala
@@ -0,0 +1,8 @@
+package com.softwaremill.codebrag.service.diff
+
+import org.scalatest.FlatSpec
+
+class DiffServiceSpec extends FlatSpec {
+  behavior of "Diff Service"
+
+}
