6726fb245a88ac571da7a036e467800093bbc914
Piotr Buda <pibuda@gmail.com>
2013-03-27 17:09:00 +0100

    Add Diff service implementation
    Add possibility to parse diff and extract each line with it's content and line number

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/diff/DiffService.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/diff/DiffService.scala
@@ -2,4 +2,24 @@ package com.softwaremill.codebrag.service.diff
 
 class DiffService {
 
+  val Info = """@@ -(\d+),(\d+) \+(\d+),(\d+) @@""".r
+
+  def parseDiff(diff: String): List[DiffLine] = {
+    def convertToDiffLines(lines: List[String], lineNumber: Int): List[DiffLine] = {
+      if (lines.isEmpty) {
+        List()
+      } else {
+        lines.head match {
+          case line@Info(startOld, countOld, startNew, countNew) => DiffLine(line, 0) :: convertToDiffLines(lines.tail, startOld.toInt)
+          case line => DiffLine(line, lineNumber) :: convertToDiffLines(lines.tail, lineNumber + 1)
+        }
+      }
+    }
+
+    val diffLines = diff.split("\n").toList
+
+    convertToDiffLines(diffLines, 0)
+  }
 }
+
+case class DiffLine(line: String, lineNumber: Int)
--- codebrag-service/src/test/scala/com/softwaremill/codebrag/service/diff/DiffServiceSpec.scala
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/diff/DiffServiceSpec.scala
@@ -1,8 +1,79 @@
 package com.softwaremill.codebrag.service.diff
 
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.scalatest.matchers.ShouldMatchers
 
-class DiffServiceSpec extends FlatSpec {
-  behavior of "Diff Service"
+class DiffServiceSpec extends FlatSpec with BeforeAndAfter with ShouldMatchers {
+  behavior of "Diff Service for changes in one file"
 
+  val SampleDiff =
+    """@@ -2,7 +2,7 @@
+      | {
+      |        "user":
+      |        {
+      |-               "login":"foo",
+      |+               "login":"foobar",
+      |                "pass":"2hf23jbd23d2839f2kejdn",
+      |                "passsalt":"1231e123123",
+      |                "id":1
+      |@@ -47,6 +47,6 @@
+      |                 "passsalt":"1231e123123",
+      |                 "id":1
+      |         },
+      |-        "joined":"1900-01-01"
+      |+        "joined":"1900-01-02"
+      | }
+      | ]""".stripMargin
+
+  var service: DiffService = _
+
+  before {
+    service = new DiffService
+  }
+
+  it should "produce data for each line in diff" in {
+    val lines = service.parseDiff(SampleDiff)
+
+    lines should have size (17)
+  }
+
+  it should "contain proper number of rows with information headers" in {
+    val lines = service.parseDiff(SampleDiff)
+
+    val infoLines = lines.filter(_.line.startsWith("@@"))
+
+    infoLines should have size (2)
+  }
+
+  it should "contain proper number of removed lines" in {
+    val lines = service.parseDiff(SampleDiff)
+
+    val removedLines = lines.filter(_.line.startsWith("-"))
+
+    removedLines should have size (2)
+  }
+
+  it should "container proper number of added lines" in {
+    val lines = service.parseDiff(SampleDiff)
+
+    val addedLines = lines.filter(_.line.startsWith("+"))
+
+    addedLines should have size (2)
+  }
+
+  it should "assign proper line numbers to diff lines" in {
+    val lines = service.parseDiff(SampleDiff)
+
+    lines(0).lineNumber should be(0)
+
+    for (i <- 0 to 7) {
+      lines(1 + i).lineNumber should be(2 + i)
+    }
+
+    lines(9).lineNumber should be(0)
+
+    for {i <- 0 to 6} {
+      lines(10 + i).lineNumber should be(47 + i)
+    }
+  }
 }
