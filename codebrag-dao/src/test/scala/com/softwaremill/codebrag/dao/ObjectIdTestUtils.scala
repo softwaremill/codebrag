d23354af62c916dffa0de9ccc94fa9a3c5668704
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-03-29 14:55:37 +0100

    Allow wider number range for number->ObjectId utility converter

--- codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/ObjectIdTestUtils.scala
+++ codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/ObjectIdTestUtils.scala
@@ -3,7 +3,14 @@ package com.softwaremill.codebrag.dao
 import org.bson.types.ObjectId
 
 object ObjectIdTestUtils {
-  implicit def intSuffixToObjId(number: Int) = new ObjectId(intSuffixToStringId(number))
-  implicit def intSuffixToStringId(number: Int) = "507f191e810c19729de860e" + number
+
+  implicit def intSuffixToObjId(number: Long) = new ObjectId(intSuffixToStringId(number))
+
+  implicit def intSuffixToStringId(number: Long) = {
+
+    val fullPrefix = "507f191e810c19729de860e"
+    val prefixLength = fullPrefix.length - number.toString.length
+    fullPrefix.substring(0, prefixLength + 1) + number
+  }
 
 }
