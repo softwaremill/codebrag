b8c656dbdffebaf825dd78e0389006327c23b33b
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-03-29 14:04:24 +0100

    Implement server side of fetching of comment list

--- codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/reporting/MongoCommentListFinder.scala
+++ codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/reporting/MongoCommentListFinder.scala
@@ -1,10 +1,32 @@
 package com.softwaremill.codebrag.dao.reporting
 
 import org.bson.types.ObjectId
+import com.softwaremill.codebrag.dao.{CommentRecord, UserRecord, CommitReviewRecord}
+import com.foursquare.rogue.LiftRogue._
 
 class MongoCommentListFinder extends CommentListFinder {
 
+
   override def findAllForCommit(commitId: ObjectId) = {
-    CommentListDTO(List.empty);
+    val recordOption = CommitReviewRecord.where(_.id eqs commitId).get()
+    CommentListDTO(
+      recordOption match {
+        case None => List.empty
+        case Some(record) => buildCommentsFromRecord(record)
+      })
+  }
+
+  private def buildCommentsFromRecord(record: CommitReviewRecord): List[CommentListItemDTO] = {
+
+    def buildCommentItem(record: CommentRecord, namesGroupedById: Map[ObjectId, String]): CommentListItemDTO = {
+      val userName = namesGroupedById.getOrElse(record.authorId.get, "Unknown user")
+      CommentListItemDTO(record.id.get.toString, userName, record.message.get, record.date.get)
+    }
+
+    val comments = record.comments.get.sortBy(_.date.get)
+    val authorIdList = comments.map(_.authorId.get)
+    val idNamesPairs = UserRecord.select(_.id, _.name).where(_.id in authorIdList).fetch
+    val namesGroupedById = idNamesPairs.toMap
+    comments.map(buildCommentItem(_, namesGroupedById))
   }
 }
---
+++ codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/reporting/MongoCommentListFinderTest.scala
@@ -0,0 +1,78 @@
+package com.softwaremill.codebrag.dao.reporting
+
+import com.softwaremill.codebrag.dao.ObjectIdTestUtils._
+import com.softwaremill.codebrag.dao._
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.matchers.ShouldMatchers
+import com.softwaremill.codebrag.domain.{Authentication, User, CommitComment, CommitReview}
+import org.joda.time.DateTime
+
+class MongoCommentListFinderTest extends FlatSpecWithMongo with BeforeAndAfterEach with ShouldMatchers {
+
+  behavior of "MongoCommentListFinder"
+  var commentListFinder: MongoCommentListFinder = _
+  val reviewedCommitId = 2
+  val comment1Id = 0
+  val comment2Id = 1
+
+  val comment1Time = new DateTime().minusHours(2)
+  val comment2Time = new DateTime().minusHours(1)
+  val userSofoklesId = 1
+  val userRobertId = 2
+
+  override def beforeEach = {
+    CommitReviewRecord.drop
+    UserRecord.drop
+    commentListFinder = new MongoCommentListFinder
+  }
+
+  it should "load empty list if there's no review for a commit" in {
+    // given
+    val dummyCommitId = 1
+
+    // when
+    val commentList = commentListFinder.findAllForCommit(dummyCommitId)
+
+    // then
+    commentList.comments should be ('empty)
+  }
+
+  it should "load comments in descending order" in {
+    // given
+    val comment1 = CommitComment(comment1Id, userSofoklesId, "nice!", comment1Time)
+    val comment2 = CommitComment(comment2Id, userRobertId, "indeed", comment2Time)
+    val review = CommitReview(reviewedCommitId, List(comment2, comment1))
+    new MongoCommitReviewDAO().save(review)
+    new MongoUserDAO().add(userWithLogin(userSofoklesId, "sofokles"))
+    new MongoUserDAO().add(userWithLogin(userRobertId, "robert"))
+
+    // when
+    val commentList = commentListFinder.findAllForCommit(reviewedCommitId)
+
+    // then
+    commentList.comments.size should equal (2)
+    commentList.comments(0) should equal (CommentListItemDTO(comment1Id, "sofokles", "nice!", comment1Time.toDate))
+    commentList.comments(1) should equal (CommentListItemDTO(comment2Id, "robert", "indeed", comment2Time.toDate))
+  }
+
+  it should "set unknown user name if he does not exist" in {
+    // given
+    val comment = CommitComment(comment1Id, userRobertId, "good one", comment1Time)
+    val review = CommitReview(reviewedCommitId, List(comment))
+    new MongoCommitReviewDAO().save(review)
+
+    // when
+    val commentList = commentListFinder.findAllForCommit(reviewedCommitId)
+
+    // then
+    commentList.comments.size should equal (1)
+    commentList.comments(0) should equal (CommentListItemDTO(comment1Id, "Unknown user", "good one", comment1Time.toDate))
+  }
+
+
+
+  def userWithLogin(idDigit: Int, login: String) = {
+    val authentication: Authentication = Authentication("Basic", login, login, "token", "salt")
+    User(idDigit, authentication, login, login + "@sml.com", "token")
+  }
+}
