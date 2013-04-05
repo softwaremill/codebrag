0120a217c57475098c0c236c14f228431dd32a69
Piotr Buda <pibuda@gmail.com>
2013-04-05 10:27:54 +0200

    Instead of changing spaces to nbsp just display data in a 'pre' formatted element.

--- codebrag-ui/src/main/webapp/scripts/commits/commitDetailsCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitDetailsCtrl.js
@@ -6,23 +6,10 @@ angular.module('codebrag.commits')
 
         if (currentCommit.isSelected()) {
             Files.get({id: currentCommit.id}, function (files) {
-                $scope.files = preprocessFiles(files);
+                $scope.files = files;
             }, function (error) {
                 console.error(error);
             });
-        }
-
-        function preprocessFiles(files) {
-            var numOfFiles = files.length;
-            for (var i = 0; i < numOfFiles; i++) {
-                var file = files[i];
-                var numOfLines = file.lines.length;
-                for (var j = 0; j < numOfLines; j++) {
-                    var line = file.lines[j];
-                    line.line = line.line.replace(/ /g, "&nbsp;");
-                }
-            }
-            return files;
         }
 
     });
