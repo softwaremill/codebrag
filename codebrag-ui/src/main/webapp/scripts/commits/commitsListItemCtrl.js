bc115ec422eb134b167a430fa3bdedcc26ccb55f
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-04-17 14:57:26 +0200

    Mark commit as reviewed - frontend, initial version

--- codebrag-ui/src/main/webapp/scripts/commits/commitsListItemCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitsListItemCtrl.js
@@ -1,9 +1,18 @@
 angular.module('codebrag.commits')
 
-    .controller('CommitsListItemCtrl', function($state, $scope) {
+    .controller('CommitsListItemCtrl', function($state, $scope, PendingCommits) {
 
         $scope.openCommitDetails = function(commit) {
             $state.transitionTo('commits.details', {id: commit.id});
+        }
+
+        $scope.markAsReviewed = function(commit, $event) {
+            $event.stopPropagation();
+            PendingCommits.delete({id: commit.id}, function() {
+                console.log('should be removed');
+                var itemIndex = $scope.commits.indexOf(commit);
+                $scope.commits.splice(itemIndex, 1);
+            })
         }
 
     });
