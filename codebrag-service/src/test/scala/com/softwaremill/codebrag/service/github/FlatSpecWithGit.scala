3681da3e90f7ed20d2e1f6d390d8eb581e4e9740
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-04-19 15:20:06 +0200

    Add some integration tests for importing data with jgit

---
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/github/FlatSpecWithGit.scala
@@ -0,0 +1,67 @@
+package com.softwaremill.codebrag.service.github
+
+import org.scalatest.{BeforeAndAfter, FlatSpec}
+import org.scalatest.matchers.ShouldMatchers
+import java.io.{PrintWriter, File}
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.{PersonIdent, Constants}
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.util.FileUtils
+import com.softwaremill.codebrag.service.github.jgit.InternalGitDirTree
+
+trait FlatSpecWithGit extends FlatSpec with BeforeAndAfter with ShouldMatchers {
+
+  var testRepo: File = _
+
+  def credentials = new UsernamePasswordCredentialsProvider("codebrag-user", "")
+
+  def uriBuildFunction = (owner: String, repoName: String) => testRepoPath
+
+  def testRepoPath = testRepo.getCanonicalPath
+
+  def initRepo(): File = {
+    val tmpDir = System.getProperty("java.io.tmpdir")
+    tmpDir should not be (null)
+    val dir = new File(tmpDir, "codebrag-test-case-" + System.nanoTime())
+    dir.mkdir() should be(true)
+    Git.init().setDirectory(dir).setBare(false).call()
+    val repo = new File(dir, Constants.DOT_GIT)
+    repo.exists() should be(true)
+    repo.deleteOnExit()
+    repo
+  }
+
+  def givenCommit(path: String, content: String, message: String): RevCommit = {
+    add(testRepo, path, content, message)
+  }
+
+  val author = new PersonIdent("Sofokles", "sofokles@softwaremill.com")
+  val committer = new PersonIdent("Bruce", "bruce@softwaremill.com")
+
+  def add(repo: File, path: String, content: String, message: String): RevCommit = {
+    val file = new File(repo.getParentFile, path)
+    if (!file.getParentFile.exists())
+      file.getParentFile.mkdirs() should be(true)
+    if (!file.exists())
+      file.createNewFile() should be(true)
+    val writer = new PrintWriter(file)
+    try {
+      writer.print(content)
+    } finally {
+      writer.close()
+    }
+    val git = Git.open(repo)
+    git.add().addFilepattern(path).call()
+    val commit = git.commit().setOnly(path).setMessage(message)
+      .setAuthor(author).setCommitter(committer).call()
+    commit should not be (null)
+    commit
+  }
+
+  def deleteRootDirectoryRecursively() {
+    FileUtils.delete(new File(InternalGitDirTree.Root), FileUtils.RECURSIVE | FileUtils.SKIP_MISSING)
+  }
+
+
+}
---
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/github/jgit/JgitGitHubCommitImporterSpec.scala
@@ -0,0 +1,113 @@
+package com.softwaremill.codebrag.service.github.jgit
+
+import org.scalatest.mock.MockitoSugar
+import com.softwaremill.codebrag.service.github.{FlatSpecWithGit, CommitReviewTaskGenerator, GitHubCommitImportService}
+import com.softwaremill.codebrag.dao.CommitInfoDAO
+import org.mockito.ArgumentMatcher
+import org.mockito.Mockito._
+import com.softwaremill.codebrag.domain.{CommitFileInfo, CommitInfo}
+import org.joda.time.DateTime
+import org.bson.types.ObjectId
+import org.mockito.Matchers._
+
+class JgitGitHubCommitImporterSpec extends FlatSpecWithGit with MockitoSugar {
+
+  var commitInfoDaoMock: CommitInfoDAO = _
+  var reviewTaskGeneratorMock: CommitReviewTaskGenerator = _
+  var service: GitHubCommitImportService = _
+  var supplementaryService: GitHubCommitImportService = _
+  val commitInfoDaoSupplementaryStub = mock[CommitInfoDAO]
+
+  before {
+    deleteRootDirectoryRecursively()
+    testRepo = initRepo()
+    commitInfoDaoMock = mock[CommitInfoDAO]
+    reviewTaskGeneratorMock = mock[CommitReviewTaskGenerator]
+    service = createService(commitInfoDaoMock)
+    supplementaryService = createService(commitInfoDaoSupplementaryStub)
+  }
+
+  after {
+    deleteRootDirectoryRecursively()
+  }
+
+  behavior of "JgitGitHubCommitImporter"
+
+  it should "call persistence to save expected commit data" in {
+    // given
+    val revCommit = givenCommit("file.txt", "file1 content", "commit1 msg")
+    val sha = revCommit.toObjectId.name
+    val commitTime = new DateTime(revCommit.getCommitTime * 1000l)
+    val expectedPatch = "diff --git a/file.txt b/file.txt\nnew file mode 100644\nindex 0000000..2e80f50\n--- /dev/null\n+++ b/file.txt\n@@ -0,0 +1 @@\n+file1 content\n\\ No newline at end of file\n"
+    val expectedCommit = CommitInfo(sha, "commit1 msg", author.getName, committer.getName,
+      commitTime, List(), List(CommitFileInfo("file.txt", "added", expectedPatch)))
+
+    // when
+    service.importRepoCommits("codebragUser", "remoteRepoName")
+
+    // then
+    verify(commitInfoDaoMock).storeCommit(argThat(IsCommitInfoIgnoringId(expectedCommit)))
+  }
+
+  it should "call persistence to save empty file" in {
+    // given
+    val revCommit = givenCommit("file.txt", "", "commit1 msg")
+    val sha = revCommit.toObjectId.name
+    val commitTime = new DateTime(revCommit.getCommitTime * 1000l)
+    val expectedPatch = "diff --git a/file.txt b/file.txt\nnew file mode 100644\nindex 0000000..e69de29\n--- /dev/null\n+++ b/file.txt\n"
+    val expectedCommit = CommitInfo(sha, "commit1 msg", author.getName, committer.getName,
+      commitTime, List(), List(CommitFileInfo("file.txt", "added", expectedPatch)))
+
+    // when
+    service.importRepoCommits("codebragUser", "remoteRepoName")
+
+    // then
+    verify(commitInfoDaoMock).storeCommit(argThat(IsCommitInfoIgnoringId(expectedCommit)))
+  }
+
+  it should "load only new commits on second call" in {
+    // given
+    givenCommit("file.txt", "file content", "commit1 msg")
+    givenCommit("file.txt", "file content update", "commit2 msg")
+    givenAlreadyCalledImport()
+    givenCommit("file.txt", "third update content", "third update message")
+    givenCommit("file.txt", "fourth update content", "fourth update message")
+
+    // when
+    service.importRepoCommits("codebragUser", "remoteRepoName")
+
+    // then
+    verify(commitInfoDaoMock).storeCommit(argThat(IsCommitInfoWithMessage("fourth update message")))
+    verify(commitInfoDaoMock).storeCommit(argThat(IsCommitInfoWithMessage("third update message")))
+    verifyNoMoreInteractions(commitInfoDaoMock)
+  }
+
+  def givenAlreadyCalledImport() {
+    supplementaryService.importRepoCommits("codebragUser", "remoteRepoName")
+  }
+
+  def createService(commitInfoDaoMock: CommitInfoDAO) = new GitHubCommitImportService(
+    new JgitGitHubCommitsLoader(
+      new JgitFacade(credentials),
+      new InternalGitDirTree,
+      new JgitLogConverter,
+      uriBuildFunction),
+    commitInfoDaoMock,
+    reviewTaskGeneratorMock)
+}
+
+case class IsCommitInfoWithMessage(message: String) extends ArgumentMatcher[CommitInfo] {
+  override def matches(obj: Object): Boolean = {
+    val commit = obj.asInstanceOf[CommitInfo]
+    commit.message equals message
+  }
+}
+
+case class IsCommitInfoIgnoringId(otherCommit: CommitInfo) extends ArgumentMatcher[CommitInfo] {
+  val constantIrrelevantObjectId = new ObjectId("507f191e810c19729de860e1")
+
+  override def matches(obj: Object): Boolean = {
+    val commit = obj.asInstanceOf[CommitInfo]
+    commit.copy(id = constantIrrelevantObjectId) equals otherCommit.copy(id = constantIrrelevantObjectId)
+  }
+}
