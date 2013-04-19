0f5bbf45149954b837adb2b1ff71b97269d6a796
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-04-19 09:37:02 +0200

    When currently displayed commit is removed from list, change commit details to empty

--- codebrag-ui/src/main/webapp/scripts/commits/commitsListItemCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitsListItemCtrl.js
@@ -1,13 +1,21 @@
 angular.module('codebrag.commits')
 
-    .controller('CommitsListItemCtrl', function($state, $scope, commitsListService) {
+    .controller('CommitsListItemCtrl', function($state, $stateParams, $scope, commitsListService) {
 
         $scope.openCommitDetails = function(commit) {
             $state.transitionTo('commits.details', {id: commit.id});
         };
 
-        $scope.markAsReviewed = function(commit, $event) {
-			commitsListService.removeCommit(commit.id);
+        $scope.markAsReviewed = function(commit) {
+			commitsListService.removeCommit(commit.id).then(function() {
+                _getOutOfCommitDetailsIfCurrentRemoved(commit.id);
+            });
+        };
+
+        function _getOutOfCommitDetailsIfCurrentRemoved(commitId) {
+            if (commitId === $stateParams.id) {
+                $state.transitionTo('commits.list');
+            }
         }
 
     });
--- codebrag-ui/src/test/unit/specs/commits/commitsListItemCtrl-spec.js
+++ codebrag-ui/src/test/unit/specs/commits/commitsListItemCtrl-spec.js
@@ -2,7 +2,9 @@
 
 describe("CommitsListItemController", function () {
 
-    var selectedCommit = {id: 123};
+    var selectedCommit = {id: '123abc'};
+    var selectedCommitStateParams = {id: selectedCommit.id};
+    var otherCommitStateParams = {id: '321cba'};
 
     beforeEach(module('codebrag.commits'));
 
@@ -19,16 +21,53 @@ describe("CommitsListItemController", function () {
         expect($state.transitionTo).toHaveBeenCalledWith('commits.details', {id: selectedCommit.id})
     }));
 
-    it('should remove given commit when marked as reviewed', inject(function($controller, $state, commitsListService) {
+    it('should remove given commit when marked as reviewed', inject(function($controller, $state, commitsListService, $q, $rootScope) {
         // Given
         var scope = {};
+        var deferred = $q.defer();
+        deferred.resolve();
         $controller('CommitsListItemCtrl', {$scope: scope});
-        spyOn(commitsListService, 'removeCommit');
+        spyOn(commitsListService, 'removeCommit').andReturn(deferred.promise);
 
         // When
         scope.markAsReviewed(selectedCommit);
+        $rootScope.$apply();
 
         // Then
         expect(commitsListService.removeCommit).toHaveBeenCalledWith(selectedCommit.id);
+    }));
+
+    it('should get out of commit details when current commit is marked as reviewed', inject(function($controller, $state, commitsListService, $q, $rootScope) {
+        // Given
+        var scope = {};
+        var deferred = $q.defer();
+        deferred.resolve();
+        $controller('CommitsListItemCtrl', {$scope: scope, $stateParams: selectedCommitStateParams});
+        spyOn(commitsListService, 'removeCommit').andReturn(deferred.promise);
+        spyOn($state, 'transitionTo');
+
+        // When
+        scope.markAsReviewed(selectedCommit);
+        $rootScope.$apply();
+
+        // Then
+        expect($state.transitionTo).toHaveBeenCalledWith('commits.list');
+    }));
+
+    it('should not change commit details view when other commit is marked as reviewed', inject(function($controller, $state, commitsListService, $q, $rootScope) {
+        // Given
+        var scope = {};
+        var deferred = $q.defer();
+        deferred.resolve();
+        $controller('CommitsListItemCtrl', {$scope: scope, $stateParams: otherCommitStateParams});
+        spyOn(commitsListService, 'removeCommit').andReturn(deferred.promise);
+        spyOn($state, 'transitionTo');
+
+        // When
+        scope.markAsReviewed(selectedCommit);
+        $rootScope.$apply();
+
+        // Then
+        expect($state.transitionTo).not.toHaveBeenCalled();
     }));
 });
