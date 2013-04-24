897bc89c35bc549da6f58a0fb57e3c9d0c5f8713
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-04-24 09:55:29 +0200

    Sealed modified added for NewComment and CommentBase to prevent non-exhaustive matches. Also removed null-check

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/EntireCommitComment.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/EntireCommitComment.scala
@@ -3,13 +3,10 @@ package com.softwaremill.codebrag.domain
 import org.joda.time.DateTime
 import org.bson.types.ObjectId
 
-abstract class CommentBase(val id: ObjectId, val commitId: ObjectId, val authorId: ObjectId, val message: String, val postingTime: DateTime)
+abstract sealed class CommentBase(val id: ObjectId, val commitId: ObjectId, val authorId: ObjectId, val message: String, val postingTime: DateTime)
 
 case class EntireCommitComment(override val id: ObjectId, override val commitId: ObjectId, override val authorId: ObjectId, override val message: String, override val postingTime: DateTime)
   extends CommentBase(id, commitId, authorId, message, postingTime)
 
 case class InlineCommitComment(commitComment: EntireCommitComment, fileName: String, lineNumber: Int)
-  extends CommentBase(commitComment.id, commitComment.commitId, commitComment.authorId, commitComment.message, commitComment.postingTime) {
-
-  require((fileName != null && lineNumber > 0),  "Inline comments must have non-empty fileName and >0 lineNumber fields")
-}
+  extends CommentBase(commitComment.id, commitComment.commitId, commitComment.authorId, commitComment.message, commitComment.postingTime)
--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/comments/command/NewComment.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/comments/command/NewComment.scala
@@ -2,7 +2,7 @@ package com.softwaremill.codebrag.service.comments.command
 
 import org.bson.types.ObjectId
 
-abstract class NewComment(val commitId: ObjectId, val authorId: ObjectId, val message: String)
+abstract sealed class NewComment(val commitId: ObjectId, val authorId: ObjectId, val message: String)
 
 case class NewEntireCommitComment(override val commitId: ObjectId, override val authorId: ObjectId, override val message: String) extends  NewComment(commitId, authorId, message)
 
