a90b5ea3149c333a6df6b8f97c19b2e78c0b9671
Krzysztof Ciesielski <panciesielski@gmail.com>
2013-04-24 09:23:06 +0200

    Switch to AngularJS 1.1.4

--- codebrag-ui/src/test/unit/test.dependencies.js
+++ codebrag-ui/src/test/unit/test.dependencies.js
@@ -1,9 +1,9 @@
 // Libraries
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "jquery-1.8.2-min.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-1.1.1.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-resource-1.1.1.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-sanitize-1.1.1.js");
-EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-cookies-1.1.1.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-resource.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-sanitize.js");
+EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-cookies.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "angular-ui-states.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "bootstrap.js");
 EnvJasmine.loadGlobal(EnvJasmine.libDir + "moment.js");
@@ -12,7 +12,7 @@ EnvJasmine.loadGlobal(EnvJasmine.libDir + "lodash.js");
 // Testing libraries
 EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/require/require-2.0.6.js");
 EnvJasmine.loadGlobal(EnvJasmine.testDir + "require.conf.js");
-EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/angular/angular-mocks-1.1.1.js");
+EnvJasmine.loadGlobal(EnvJasmine.testDir + "../lib/angular/angular-mocks.js");
 
 // Application
 
