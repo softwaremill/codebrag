d8c7d1363c2a5c231b64069af48ce4c6dc87ac46
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-05-22 14:56:31 +0200

    Up to date with codebrag-design

--- codebrag-ui/src/main/webapp/scripts/commits/commitLoadFilter.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitLoadFilter.js
@@ -1,23 +1,33 @@
-angular.module('codebrag.commits').
-    value('commitLoadFilter',
-    {
-        modes: {
-            all: {
-                name: "All",
-                value: 'all'
+angular.module('codebrag.commits')
+
+    .value('commitLoadFilter',
+
+        {
+            modes: {
+                all: {
+                    name: "All",
+                    value: 'all'
+                },
+                pending: {
+                    name: "Pending review",
+                    value: 'pending'
+                }
             },
-            pending: {
-                name: "Pending review",
+
+            current: {
                 value: 'pending'
+            },
+
+            setPendingMode: function() {
+                this.current = this.modes.pending;
+            },
+
+            setAllMode: function() {
+                this.current = this.modes.all;
+            },
+
+            isAll: function () {
+                return this.current == this.modes.all;
             }
-        },
-
-        current: {
-            value: 'pending'
-        },
-
-        isAll: function () {
-            return this.current == this.modes.all;
         }
-    }
-);
+    );
--- codebrag-ui/src/main/webapp/scripts/commits/commitsCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitsCtrl.js
@@ -2,15 +2,67 @@ angular.module('codebrag.commits')
 
     .controller('CommitsCtrl', function ($scope, $http, commitsListService, commitLoadFilter) {
 
-        commitLoadFilter.current = commitLoadFilter.modes.pending;
+        $scope.filter = {
 
-        $scope.filter = commitLoadFilter;
-        $scope.syncCommits = commitsListService.syncCommits;
-
-        $scope.loadCommits = function() {
-            commitsListService.loadCommitsFromServer($scope.filter.current);
-            $scope.commits = commitsListService.allCommits();
         };
 
-        $scope.loadCommits();
+        $scope.filter = {
+            current: {
+                value: 'pending'
+            },
+
+            setPendingMode: function() {
+                this.current.value = 'pending';
+            },
+
+            setAllMode: function() {
+                this.current.value = 'all';
+            },
+
+            isInPendingMode: function() {
+                return this.current.value === 'pending';
+            },
+
+            isInAllMode: function() {
+                return this.current.value === 'all';
+            }
+        };
+
+        $scope.syncCommits = commitsListService.syncCommits;
+
+        function loadCommits() {
+            commitsListService.loadCommitsFromServer($scope.filter.current);
+            $scope.commits = commitsListService.allCommits();
+        }
+
+        $scope.filterList = function(value) {
+            if(filter.is)
+            $scope.filter.setPendingMode();
+            loadCommits();
+        };
+
+        $scope.showAll = function() {
+            $scope.filter.setAllMode();
+            loadCommits();
+        };
+
+        loadCommits();
+
+    })
+
+    .directive('styleAsSelectedWhen', function() {
+
+        return {
+            restrict: 'A',
+            link: function(scope, el, attrs) {
+
+                scope.$watch(attrs.styleAsSelectedWhen, function(selected) {
+                    if(selected) {
+                        el.removeClass('link');
+                    } else {
+                        el.addClass('link');
+                    }
+                });
+            }
+        }
     });
