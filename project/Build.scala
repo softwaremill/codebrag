e5f4ba8ecf96cb7cb1e3176999133645780e75fe
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-05-27 22:26:50 +0200

    Switch dependencies related to LiftRecord to newer RC versions

--- project/Build.scala
+++ project/Build.scala
@@ -60,7 +60,7 @@ object Dependencies {
   val logBackVersion = "1.0.9"
   val smlCommonVersion = "72"
   val scalatraVersion = "2.2.0"
-  val rogueVersion = "2.0.0-RC1"
+  val rogueVersion = "2.0.0-RC4"
   val scalaLoggingVersion = "1.0.1"
 
   val slf4jApi = "org.slf4j" % "slf4j-api" % slf4jVersion
@@ -124,8 +124,9 @@ object Dependencies {
   val rogueField = "com.foursquare" %% "rogue-field" % rogueVersion intransitive()
   val rogueCore = "com.foursquare" %% "rogue-core" % rogueVersion intransitive()
   val rogueLift = "com.foursquare" %% "rogue-lift" % rogueVersion intransitive()
-  val liftMongoRecord = "net.liftweb" %% "lift-mongodb-record" % "2.5-RC4"
-  val rogue = Seq(rogueCore, rogueField, rogueLift, liftMongoRecord)
+  val rogueIndex = "com.foursquare" %% "rogue-index" % rogueVersion intransitive()
+  val liftMongoRecord = "net.liftweb" %% "lift-mongodb-record" % "2.5-RC6"
+  val rogue = Seq(rogueCore, rogueField, rogueLift, rogueIndex, liftMongoRecord)
 
   val egitGithubApi = "org.eclipse.mylyn.github" % "org.eclipse.egit.github.core" % "2.1.3"
   val jGit = "org.eclipse.jgit" % "org.eclipse.jgit" % "2.3.1.201302201838-r"
