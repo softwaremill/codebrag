893ef83e588b5181101a13e412d72ebcc3f4f528
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-05-31 13:10:53 +0200

    Add possibility to open comment form by clicking anywhere in the diff line

--- codebrag-ui/src/main/webapp/scripts/commits/comments/commentable.js
+++ codebrag-ui/src/main/webapp/scripts/commits/comments/commentable.js
@@ -4,10 +4,10 @@ angular.module('codebrag.commits.comments')
         var template = $('#inlineCommentForm').html();
         return {
             restrict: 'A',
-            link: function(scope, element, attrs) {
+            link: function(scope, element) {
                 var alreadyOpened = false;
-                var codeLine = $('a.comment', element);
-                codeLine.bind('click', function() {
+
+                var onclick = function() {
                     if(alreadyOpened) {
                         _locateCommentFormRow().show();
                         return;
@@ -16,7 +16,12 @@ angular.module('codebrag.commits.comments')
                     $compile($('tr.comment-form', element).contents())(scope);
                     alreadyOpened = true;
                     scope.$digest();
-                });
+                };
+
+                var codeLine = $('td.diff-line-code', element);
+                var commentBox = $('a.comment', element);
+                codeLine.bind('click', onclick);
+                commentBox.bind('click', onclick);
 
                 scope.$on(events.closeForm, function(event) {
                     event.stopPropagation();
