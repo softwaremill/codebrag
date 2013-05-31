a8f87f9b8b9479930d4275293d1cc192089948fa
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-05-31 11:31:13 +0200

    Revert "Remove redundant call on app initialization to avoid duplicate checks"
    This reverts commit b1c0a4067b5771947486dd247469fbadccec056b.
    - Required to correctly 'remember' user after he opens Codebrag back after closing.

--- codebrag-ui/src/main/webapp/scripts/app.js
+++ codebrag-ui/src/main/webapp/scripts/app.js
@@ -24,6 +24,11 @@ angular.module('codebrag', [
     'codebrag.followups',
     'codebrag.notifications']);
 
+angular.module('codebrag')
+    .run(function(authService) {
+        authService.requestCurrentUser();
+    });
+
 angular.module('codebrag.auth')
     .config(function($httpProvider) {
         $httpProvider.responseInterceptors.push('httpAuthInterceptor');
