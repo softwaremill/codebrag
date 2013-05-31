0a6b17a417b2fec8ac76f4b59a47e5fa35c88d38
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-05-31 12:04:31 +0200

    Make current diff disappear when switching between pending / all

--- codebrag-ui/src/main/webapp/scripts/commits/commitsCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitsCtrl.js
@@ -1,15 +1,17 @@
 angular.module('codebrag.commits')
 
-    .controller('CommitsCtrl', function ($scope, $http, commitsListService, commitLoadFilter) {
+    .controller('CommitsCtrl', function ($scope, $http, commitsListService, commitLoadFilter, $state) {
 
         $scope.syncCommits = commitsListService.syncCommits;
 
         $scope.loadAllCommits = function() {
             $scope.commits = commitsListService.loadAllCommits();
+            $state.transitionTo('commits.list');
         };
 
         $scope.loadPendingCommits = function() {
             $scope.commits = commitsListService.loadCommitsPendingReview();
+            $state.transitionTo('commits.list');
         };
 
         $scope.canLoadMore = function() {
