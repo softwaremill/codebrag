df303c0d3cc62675a738d2551c7fa06acfbc3327
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-06-02 21:54:25 +0200

    Directives refactored

--- codebrag-ui/src/main/webapp/scripts/commits/commitDiff.js
+++ codebrag-ui/src/main/webapp/scripts/commits/commitDiff.js
@@ -5,22 +5,19 @@ angular.module('codebrag.commits')
             restrict: 'E',
             replace: true,
             compile: function(tEl, tAttrs, transclude) {
-                var template = tEl.html();
+                var diffTemplate = Handlebars.compile(tEl.html());
                 tEl.html('');
                 return function(scope, el, attrs) {
-                    var attr = attrs.data;
-                    var compileSelector = attrs.compile;
-                    var dataRoot = {};
-                    var watcherRemove = scope.$watch(attr, function(newVal) {
+                    var dataAttrName = attrs.data;
+                    var removeWatcher = scope.$watch(dataAttrName, function(newVal) {
+                        var dataRoot = {};
                         if(angular.isUndefined(newVal)) {
                             return;
                         }
-                        dataRoot[attr] = scope.$eval(attr);
-                        var tpl = Handlebars.compile(template);
-                        var output = tpl(dataRoot);
-                        el.html(output);
-                        $compile(el.find(compileSelector))(scope);
-                        watcherRemove();
+                        dataRoot[dataAttrName] = scope.$eval(dataAttrName);
+                        el.html(diffTemplate(dataRoot));
+                        $compile(el.find(attrs.compile))(scope);
+                        removeWatcher();
                     });
                 }
             }
@@ -29,52 +26,62 @@ angular.module('codebrag.commits')
 
     .directive('inlineCommentable', function($compile, events) {
 
-        var formTemplate = $('#inlineCommentForm').html(); //$templateCache.get('inlineCommentForm');
+        var inlineCommentFormTemplate = $('#inlineCommentForm').html(); //$templateCache.get('inlineCommentForm');
 
-        function RowDOMElements(rowClicked) {
+        var fileDiffRootSelector = 'table';
+        var fileDiffLineSelector = 'tbody';
+        var clickableSelector = '[data-commentable]';
+        var inlineCommentFormRootSelector = 'tr.comment-form';
 
-            var tbody = rowClicked.parents('tbody');
-            var table = rowClicked.parents('table');
+        var fileNameDataAttr = 'file-name';
+        var lineNumberDataAttr = 'line-number';
 
-            this.insertCommentForm = function(template) {
-                tbody.append(template);
+        function InlineCommentForm(rowClicked, fileDiffRoot) {
+
+            var fileDiffLine = rowClicked.parents(fileDiffLineSelector);
+
+            this.insert = function(afterFormInsertCallback) {
+                fileDiffLine.append(inlineCommentFormTemplate);
+                var insertedElement = fileDiffLine.find(inlineCommentFormRootSelector);
+                afterFormInsertCallback(insertedElement);
             };
 
-            this.removeCommentForm = function() {
-                this.getCommentFormRowInserted().remove();
+            this.destroy = function(afterFormDestroyCallback) {
+                fileDiffLine.find(inlineCommentFormRootSelector).remove();
+                afterFormDestroyCallback();
             };
 
-            this.getCommentFormRowInserted = function() {
-                return tbody.find('tr.comment-form')
-            };
-
-            this.getFileAndLine = function() {
+            this.commentParams = function() {
                 return {
-                    fileName: table.data('file-name'),
-                    lineNumber: tbody.data('line-number')
+                    fileName: fileDiffRoot.data(fileNameDataAttr),
+                    lineNumber: fileDiffLine.data(lineNumberDataAttr)
                 }
             };
 
-        }
+            this.isAlreadyPresent = function() {
+                return fileDiffLine.find(inlineCommentFormRootSelector).length > 0;
+            }
 
-        function insertFormWithNewScope(domElements, baseScope) {
-            domElements.insertCommentForm(formTemplate);
-            var formScope = baseScope.$new();
-            formScope.commentData = domElements.getFileAndLine();
-            $compile(domElements.getCommentFormRowInserted())(formScope);
-            formScope.$apply();
-            return formScope;
         }
 
         function linkFn(scope, el, attrs) {
-            var table = el.parent('table');
-            table.on('click', '[data-commentable="true"]', function(event) {
-                var rowClicked = $(event.currentTarget);
-                var domElements = new RowDOMElements(rowClicked);
-                var formScope = insertFormWithNewScope(domElements, scope);
-                formScope.$on(events.closeForm, function() {
-                    domElements.removeCommentForm();
-                    formScope.$destroy();
+            var fileDiffRoot = el.parent(fileDiffRootSelector);
+            fileDiffRoot.on('click', clickableSelector, function(event) {
+                var commentFormScope = scope.$new();
+                var elementClicked = $(event.currentTarget);
+                var commentForm = new InlineCommentForm(elementClicked, fileDiffRoot);
+                if(commentForm.isAlreadyPresent()) {
+                    return;
+                }
+                commentForm.insert(function bindScope(createdElement) {
+                    commentFormScope.commentParams = commentForm.commentParams();
+                    $compile(createdElement)(commentFormScope);
+                    commentFormScope.$apply();
+                });
+                commentFormScope.$on(events.closeForm, function() {
+                    commentForm.destroy(function destroyScope() {
+                        commentFormScope.$destroy();
+                    });
                 })
             });
         }
@@ -83,7 +90,65 @@ angular.module('codebrag.commits')
             restrict: 'A',
             link: linkFn
         }
+    })
+
+
+    .directive('liveComments', function($compile) {
+
+        var commentTemplate = $('#inlineComment').html();    // templateCache ???
+
+        var fileDiffRootSelector = 'table';
+        var lineCommentsListSelector = '.inline-comments-container';
+
+        var lineNumberDataAttr = 'line-number';
+
+        function FileInlineComments(baseElement, fileInlineComments) {
+
+            this.insert = function(afterDOMInsertCallback) {
+                _.forEach(fileInlineComments, function(lineComments, lineNumber) {
+                    var diffLine = _getCorrespondingLineDOMElement(lineNumber);
+                    if(_lineHasNoCommentsYet(diffLine)) {
+                        diffLine.append(commentTemplate);
+                        afterDOMInsertCallback(diffLine, lineComments);
+                    }
+                });
+            };
+
+            function _lineHasNoCommentsYet(line) {
+                return line.find(lineCommentsListSelector).length === 0;
+            }
+
+            function _getCorrespondingLineDOMElement(lineNumber) {
+                var lineNumberSelector = ['[data-', lineNumberDataAttr, '="', lineNumber + '"]'].join(""); // create [data-line-number="xx"] selector
+                return baseElement.find(lineNumberSelector);
+            }
+
+        }
+
+        function linkFn(scope, el, attrs) {
+
+            var fileDiffRoot = el.parent(fileDiffRootSelector);
+
+            scope.$watch(attrs.liveComments, function(fileComments) {
+                if(angular.isUndefined(fileComments)) {
+                    return;
+                }
+                var comments = new FileInlineComments(fileDiffRoot, fileComments);
+                comments.insert(function(line, lineComments) {
+                    var newScope = scope.$new();
+                    newScope.lineComments = lineComments;
+                    $compile(line)(newScope);
+                });
+            }, true);
+        }
+
+        return {
+            restrict: 'A',
+            link: linkFn
+        }
+
     });
+
 
 
 
--- codebrag-ui/src/main/webapp/scripts/commits/diffCtrl.js
+++ codebrag-ui/src/main/webapp/scripts/commits/diffCtrl.js
@@ -2,17 +2,37 @@ angular.module('codebrag.commits')
 
     .controller('DiffCtrl', function ($scope, Comments) {
 
+        $scope.userComments = {
+            'codebrag-ui-grunt/README.md': {
+                4: [
+                    {id: '123', authorName: 'Stefan', message: 'test msg'}
+                ],
+                10: [
+                    {id: '123', authorName: 'Stefan', message: 'test msg for 10th line'}
+                ]
+            }
+        };
+
         $scope.submitInlineComment = function(content, commentData) {
-            var comment = {
+            var newComment = {
                 commitId: $scope.currentCommit.commit.id,
                 body: content,
                 fileName: commentData.fileName,
                 lineNumber: commentData.lineNumber
             };
-            console.log(comment);
-//            return Comments.save(comment).$then(function (commentResponse) {
-//                line.comments.push(commentResponse.data.comment);
-//            });
+
+            return Comments.save(newComment).$then(function (commentResponse) {
+                var comment = commentResponse.data.comment;
+
+                comment.fileName = newComment.fileName;
+                comment.lineNumber = newComment.lineNumber;
+
+                if(_.isUndefined($scope.userComments[comment.fileName][comment.lineNumber])) {
+                    $scope.userComments[comment.fileName][comment.lineNumber] = [];
+                }
+                $scope.userComments[comment.fileName][comment.lineNumber].push(comment);
+                $scope.$broadcast('commentAdded', comment);
+            });
         };
 
         $scope.submitComment = function (content) {
