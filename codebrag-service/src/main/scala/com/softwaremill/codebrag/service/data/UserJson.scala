0f74fa6e718472b29341681b1ffee602768d9c79
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-06-04 09:39:08 +0200

    User gets default avatar in case of missing specific one

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/data/UserJson.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/data/UserJson.scala
@@ -5,7 +5,17 @@ import com.softwaremill.codebrag.domain.User
 case class UserJson(id: String, login: String, fullName: String, email:String, token: String, avatarUrl: String)
 
 object UserJson {
-  def apply(user: User) = new UserJson(user.id.toString, user.authentication.username, user.name, user.email, user.token, user.avatarUrl)
+  def apply(user: User) = {
+    new UserJson(user.id.toString, user.authentication.username, user.name, user.email, user.token, userAvatarUrl(user))
+  }
+
+  private def userAvatarUrl(user: User): String = {
+    if (user.avatarUrl.isEmpty) {
+      User.defaultAvatarUrl(user.email)
+    } else {
+      user.avatarUrl
+    }
+  }
 
   def apply(list: List[User]): List[UserJson] = {
     for (user <- list) yield UserJson(user)
