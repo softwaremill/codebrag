54efca3f4c056fda76fc504604a48cdf6690d849
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-06-20 12:28:14 +0200

    Improve handling of missing user on intialization of commit importer

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/github/jgit/JgitGitHubCommitImportServiceFactory.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/github/jgit/JgitGitHubCommitImportServiceFactory.scala
@@ -5,15 +5,24 @@ import com.softwaremill.codebrag.dao.{UserDAO, CommitInfoDAO}
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import com.softwaremill.codebrag.common.EventBus
 import com.softwaremill.codebrag.service.config.CodebragConfig
+import com.typesafe.scalalogging.slf4j.Logging
 
 class JgitGitHubCommitImportServiceFactory(commitInfoDao: CommitInfoDAO,
                                            userDao: UserDAO,
                                            eventBus: EventBus,
-                                           codebragConfiguration: CodebragConfig) extends GitHubCommitImportServiceFactory {
+                                           codebragConfiguration: CodebragConfig) extends GitHubCommitImportServiceFactory with Logging {
 
   def createInstance(login: String): GitHubCommitImportService = {
-    val importingUserToken = userDao.findByLoginOrEmail(login).get.authentication.token
-    val credentials = new UsernamePasswordCredentialsProvider(importingUserToken, "")
+    val importingUserOpt = userDao.findByLoginOrEmail(login)
+
+      val token = importingUserOpt match {
+        case Some(user) => user.authentication.token
+        case None => {
+          logger.warn("User $login not found in DB. Cannot properly initialize commit importer")
+          s"user-$login-not-found"
+        }
+      }
+    val credentials = new UsernamePasswordCredentialsProvider(token, "")
     val uriBuilder = new GitHubRemoteUriBuilder
     new GitHubCommitImportService(
       new JgitGitHubCommitsLoader(
