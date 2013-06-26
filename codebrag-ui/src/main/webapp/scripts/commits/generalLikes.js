db90f305685b50b0e66133a30d879b30f4c6df10
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2013-06-26 15:34:14 +0200

    Apply new styles to general likes - also update the directive to properly change DOM on data change

--- codebrag-ui/src/main/webapp/scripts/commits/generalLikes.js
+++ codebrag-ui/src/main/webapp/scripts/commits/generalLikes.js
@@ -10,38 +10,50 @@ angular.module('codebrag.commits')
                 submitLike: '&'
             },
             templateUrl: 'generalLikes',
-            link: function (scope) {
+            link: function (scope, element) {
 
                 scope.noLikes = function () {
                     return _.isEmpty(scope.likesCollection)
                 };
+                scope.conjugatedLike = "likes";
+
                 var currentUserName = authService.loggedInUser.fullName;
 
-                var removeAuthorWatch = scope.$watch(function() { return scope.author }, function(newVal) {
+                var setUserAlreadyLikedStyle = function (isAlreadyLiked) {
+                    if (isAlreadyLiked) $('div.like-commit', element).addClass("liked-by-user");
+                };
+
+                var removeAuthorWatch = scope.$watch(function () {
+                    return scope.author
+                }, function (newVal) {
                     if (angular.isUndefined(newVal)) {
                         return;
                     }
-                    scope.userAllowedToLike = scope.author !== currentUserName;
+                    setUserAlreadyLikedStyle(scope.author == currentUserName);
                     removeAuthorWatch();
                 });
 
-                scope.$watch(function () {
-                        return scope.likesCollection
-                    }, function (newVal) {
-                        if (angular.isUndefined(newVal)) {
-                            return;
-                        }
-                        if (scope.userAllowedToLike) {
-
+                var removeCollectionReferenceWatch = scope.$watch(function () {
+                    return scope.likesCollection
+                }, function (newVal) {
+                    if (angular.isUndefined(newVal)) {
+                        return;
+                    }
+                    scope.$watch(function () {
+                            return scope.likesCollection.length;
+                        }, function (newVal) {
+                            if (newVal > 1) {
+                                scope.conjugatedLike = "like";
+                            }
                             if (_.some(scope.likesCollection, function (like) {
                                 return like.authorName == currentUserName
                             })) {
-                                scope.userAllowedToLike = false;
+                                setUserAlreadyLikedStyle(true);
                             }
                         }
-                    }
-                )
-                ;
+                    );
+                    removeCollectionReferenceWatch();
+                });
             }
         }
 
