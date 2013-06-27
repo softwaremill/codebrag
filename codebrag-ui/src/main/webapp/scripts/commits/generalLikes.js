941053b7e7a5f94638b2c2bb6a9dd5b76587ac2c
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-06-27 09:16:33 +0200

    Revert back not replacing content for general likes directive

--- codebrag-ui/src/main/webapp/scripts/commits/generalLikes.js
+++ codebrag-ui/src/main/webapp/scripts/commits/generalLikes.js
@@ -4,7 +4,6 @@ angular.module('codebrag.commits')
 
         return {
             restrict: 'E',
-            replace: true,
             scope: {
                 author: '=',
                 likesCollection: '=',
