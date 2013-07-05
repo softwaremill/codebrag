b64a7fa5395c748cf759eb2a42002170cbde48de
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-07-05 12:08:48 +0200

    Fix not displaying trophies when commits/followups are not yet loaded. Also update db dumps

--- scripts/demo/resetAll.sh
+++ scripts/demo/resetAll.sh
@@ -1,7 +1,7 @@
 # Script for restoring database state from a dump file on TechCrunch demo server
 #!/bin/bash
-mv ~/techcrunch-codebrag-dump.tar.gz ~/mongo/bin
+mv ~/codebrag-dump.tar.gz ~/mongo/bin
 cd ~/mongo/bin
-tar xvf techcrunch-codebrag-dump.tar.gz
+tar xvf codebrag-dump.tar.gz
 ./mongo --eval "codebrag.dropDatabase()"
 ./mongorestore dump
