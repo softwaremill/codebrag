0cfb13530acc3091b34616af622436f2d819726d
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-07-10 20:30:56 +0200

    Add NewFollowup (temporary name) with ability to build from the old one

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/Followup.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/Followup.scala
@@ -3,7 +3,27 @@ package com.softwaremill.codebrag.domain
 import org.bson.types.ObjectId
 import org.joda.time.DateTime
 
+case class NewFollowup(receivingUserId: ObjectId, reaction: UserReaction)
 
+object NewFollowup {
+
+  def fromOldFollowup(followup: Followup) = {
+    val f = followup
+    val reaction = followup.followupType match {
+      case Followup.FollowupType.Comment => {
+        Comment(f.reactionId, f.threadId.commitId, f.authorId, f.date, "", f.threadId.fileName, f.threadId.lineNumber)
+      }
+      case Followup.FollowupType.Like => {
+        Like(f.reactionId, f.threadId.commitId, f.authorId, f.date, f.threadId.fileName, f.threadId.lineNumber)
+      }
+    }
+    new NewFollowup(followup.userId, reaction)
+  }
+
+}
+
+@deprecated
+// will be deprecated in favor of NewFollowup - better describes what followup is and is lot cleaner
 case class Followup(reactionId: ObjectId, authorId: ObjectId, userId: ObjectId, date: DateTime, lastCommenterName: String, threadId: ThreadDetails, followupType: Followup.FollowupType.Value) {
 
   def isOwner(userId: ObjectId) = {
