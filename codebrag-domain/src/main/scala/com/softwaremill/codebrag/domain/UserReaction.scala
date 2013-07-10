80e2585d45fdee726b555ac3721451536690218f
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-07-10 20:18:23 +0200

    UserReaction has Enum indicating its type (Like or Comment)

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/UserReaction.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/UserReaction.scala
@@ -2,10 +2,19 @@ package com.softwaremill.codebrag.domain
 
 import org.joda.time.DateTime
 import org.bson.types.ObjectId
+import scala.Enumeration
 
 abstract sealed class UserReaction(val id: ObjectId, val commitId: ObjectId, val authorId: ObjectId, val postingTime: DateTime, val fileName: Option[String] = None, val lineNumber: Option[Int] = None) {
 
   def threadId = ThreadDetails(commitId, lineNumber, fileName)
+
+  def reactionType: UserReactionTypeEnum.Value
+
+  object UserReactionTypeEnum extends Enumeration {
+    type UserReactionTypeEnum = Value
+    val Like = Value("Like")
+    val Comment = Value("Comment")
+  }
 
 }
 
@@ -17,6 +26,9 @@ case class Comment(
                       message: String,
                       override val fileName: Option[String] = None,
                       override val lineNumber: Option[Int] = None) extends UserReaction(id, commitId, authorId, postingTime, fileName, lineNumber) {
+
+  override val reactionType = UserReactionTypeEnum.Comment
+
 }
 
 case class Like(
@@ -26,6 +38,9 @@ case class Like(
                     override val postingTime: DateTime,
                     override val fileName: Option[String] = None,
                     override val lineNumber: Option[Int] = None) extends UserReaction(id, commitId, authorId, postingTime, fileName, lineNumber) {
+
+  override val reactionType = UserReactionTypeEnum.Like
+
 }
 
 case class ThreadDetails(commitId: ObjectId, lineNumber: Option[Int] = None, fileName: Option[String] = None)
