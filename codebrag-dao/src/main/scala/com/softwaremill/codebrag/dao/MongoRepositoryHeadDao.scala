4798afaa675efdf210cacce58a7213e5cf25adbe
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-07-22 12:26:09 +0200

    Place to store repository HEAD in database

---
+++ codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/MongoRepositoryHeadDao.scala
@@ -0,0 +1,53 @@
+package com.softwaremill.codebrag.dao
+
+import net.liftweb.mongodb.record.{MongoMetaRecord, MongoRecord}
+import net.liftweb.mongodb.record.field.ObjectIdPk
+import net.liftweb.json.JsonDSL._
+import com.foursquare.rogue.LiftRogue._
+import com.mongodb.DBObject
+
+class MongoRepositoryHeadDao {
+
+  def update(repoName: String, newSha: String) {
+    val query = RepositoryHeadRecord.where(_.repoName eqs repoName)
+    val updated = createUpdatedVersion(newSha, repoName)
+    RepositoryHeadRecord.upsert(query.asDBObject, updated)
+  }
+
+
+  def createUpdatedVersion(newSha: String, repoName: String): DBObject = {
+    val updated = RepositoryHeadRecord.createRecord
+      .sha(newSha)
+      .repoName(repoName)
+      .asDBObject
+    updated.removeField("_id")
+    updated
+  }
+
+  def get(repoName: String) = {
+    RepositoryHeadRecord.where(_.repoName eqs repoName).get() match {
+      case Some(record) => Some(record.sha.get)
+      case None => None
+    }
+  }
+
+}
+
+class RepositoryHeadRecord extends MongoRecord[RepositoryHeadRecord] with ObjectIdPk[RepositoryHeadRecord] {
+
+  def meta = RepositoryHeadRecord
+
+  object sha extends LongStringField(this)
+
+  object repoName extends LongStringField(this)
+
+}
+
+object RepositoryHeadRecord extends RepositoryHeadRecord with MongoMetaRecord[RepositoryHeadRecord] {
+  override def collectionName = "repo_head"
+
+  def ensureIndexes() {
+    this.ensureIndex(keys = (repoName.name -> 1), unique = true)
+  }
+}
+
---
+++ codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/MongoRepositoryHeadRecordSpec.scala
@@ -0,0 +1,50 @@
+package com.softwaremill.codebrag.dao
+
+import com.softwaremill.codebrag.test.mongo.ClearDataAfterTest
+import org.scalatest.matchers.ShouldMatchers
+
+class MongoRepositoryHeadRecordSpec extends FlatSpecWithMongo with ClearDataAfterTest with ShouldMatchers {
+
+  var repoHeadDao = new MongoRepositoryHeadDao
+
+  it should "store repository HEAD reference if no record for given repo exists" in {
+    // given
+    val repoName = "codebrag"
+    val newHeadId = "123abc"
+
+    // when
+    repoHeadDao.update(repoName, newHeadId)
+    val Some(fetched) = repoHeadDao.get(repoName)
+
+    // then
+    fetched should be(newHeadId)
+  }
+
+  it should "update repository HEAD reference if record for given repo already exists" in {
+    // given
+    val repoName = "codebrag"
+    val oldHeadId = "123abc"
+    val newHeadId = "456def"
+    repoHeadDao.update(repoName, oldHeadId)
+
+    // when
+    repoHeadDao.update(repoName, newHeadId)
+    val Some(fetched) = repoHeadDao.get(repoName)
+
+    // then
+    fetched should be(newHeadId)
+  }
+
+  it should "update have none as a result when no HEAD ref for given repo exists" in {
+    // given
+    val repoName = "codebrag"
+
+    // when
+    repoHeadDao.get(repoName)
+    val fetched = repoHeadDao.get(repoName)
+
+    // then
+    fetched should be(None)
+  }
+
+}
