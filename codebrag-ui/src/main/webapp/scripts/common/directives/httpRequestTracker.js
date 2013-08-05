f9e5101e1f11d1c473f7410b598eeb8ad77743e6
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-08-05 14:31:54 +0200

    Request tracker can now track only specified requests (or all if no specified type provided)

--- codebrag-ui/src/main/webapp/scripts/common/directives/httpRequestTracker.js
+++ codebrag-ui/src/main/webapp/scripts/common/directives/httpRequestTracker.js
@@ -3,17 +3,30 @@ angular.module('codebrag.common.directives')
 	.directive('httpRequestTracker', function($http) {
 		return {
 			restricted: 'A',
-			scope: {},			
-			link: function(scope, element) {
-				scope.$watch(function() {
-					return $http.pendingRequests.length;
-				}, function(value) {
-					if(value > 0) {
-						element.show();
-						return;
-					}
-					element.hide();
-				});
+			scope: {},
+			link: function(scope, element, attrs) {
+                var requestType = attrs.httpRequestTracker;
+
+                function observedRequestsCount() {
+                    var pendingRequests = $http.pendingRequests;
+                    if(requestType) {
+                        return pendingRequests.filter(function(request) {
+                            return request.requestType === requestType;
+                        }).length;
+                    } else {
+                        return pendingRequests.length;
+                    }
+                }
+
+                scope.$watch(function() {
+                    return $http.pendingRequests.length
+                }, function() {
+                    if(observedRequestsCount()) {
+                        element.show();
+                        return;
+                    }
+                    element.fadeOut();
+                });
 			}
 		};
 	});
