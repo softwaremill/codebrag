3edb3f585e7b5a1a580f4355041f218140a7eb16
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-08-20 11:27:00 +0200

    Clicking on Early Version popup link opens contact form

--- codebrag-ui/src/main/webapp/scripts/addons.js
+++ codebrag-ui/src/main/webapp/scripts/addons.js
@@ -6,7 +6,6 @@ var codebrag = codebrag || {};
 codebrag.addons = {};
 
 codebrag.addons.betaRibbon = (function() {
-
     var ribbon = $('.ribbon');
     var infoPopup = $('#early-version-popup');
     var popupCloseBtn = infoPopup.find('.close-btn');
@@ -14,16 +13,19 @@ codebrag.addons.betaRibbon = (function() {
 
     var openedPopupClass = 'opened';
 
-    function togglePopupPage() {
+    function bindClicks(feedbackForm) {
         [ribbon, popupCloseBtn, link].forEach(function(el) {
             el.on('click', function() {
                 infoPopup.toggleClass(openedPopupClass);
             });
         });
+        link.on('click', function() {
+            feedbackForm.togglePopup();
+        });
     }
 
     return {
-        init: togglePopupPage
+        init: bindClicks
     }
 
 }());
@@ -34,24 +36,26 @@ codebrag.addons.feedbackForm = (function() {
     var formPopup = $('#uservoice-form-popup');
     var closeFormBtn = formPopup.find('.close-btn');
 
+    function openFormPopup() {
+        formPopup.toggleClass('opened');
+    }
+
     function togglePopupDisplay() {
         [feedbackBtn, closeFormBtn].forEach(function(e) {
             e.on('click', function() {
-                console.log('aa');
-                formPopup.toggleClass('opened');
+                openFormPopup();
             });
         });
     }
 
     return {
-        init: togglePopupDisplay
+        init: togglePopupDisplay,
+        togglePopup: openFormPopup
     }
-
 }());
 
 $(document).ready(function() {
-
-    codebrag.addons.betaRibbon.init();
     codebrag.addons.feedbackForm.init();
+    codebrag.addons.betaRibbon.init(codebrag.addons.feedbackForm);
 
 });
