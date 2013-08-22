950d92d455b78d44bd287a12ac36cf39eb4e7936
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-08-22 15:41:30 +0200

    Repository updates are not queued when update takes longer than expected.
    Now next update is scheduled only when previous one is finished

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
@@ -1,10 +1,12 @@
 package com.softwaremill.codebrag.service.updater
 
-import akka.actor.Actor
+import akka.actor._
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.service.commits.{RepoData, CommitImportService}
+import scala.Some
+import com.softwaremill.codebrag.service.updater.LocalRepositoryUpdater.UpdateCommand
 
-class LocalRepositoryUpdater(importService: CommitImportService) extends Actor with Logging {
+class LocalRepositoryUpdater(importService: CommitImportService, actorSystem: ActorSystem) extends Actor with Logging {
 
   private var repoData: Option[RepoData] = None
 
@@ -18,12 +20,21 @@ class LocalRepositoryUpdater(importService: CommitImportService) extends Actor w
         repoData.foreach(importService.importRepoCommits)
       } catch {
         case e: Exception => logger.error("Exception while importing commits", e)
+      } finally {
+        import actorSystem.dispatcher
+        logger.debug("Scheduling next update delay to " + UpdateCommand.NextUpdatesInterval.toString)
+        actorSystem.scheduler.scheduleOnce(UpdateCommand.NextUpdatesInterval, self, LocalRepositoryUpdater.UpdateCommand)
       }
     }
   }
 }
 
 object LocalRepositoryUpdater {
-  object UpdateCommand
+
+  object UpdateCommand {
+    import scala.concurrent.duration._
+    val InitialDelay = 3 seconds
+    val NextUpdatesInterval = 45 seconds
+  }
   case class RefreshRepoData(newRepoData: RepoData)
 }
--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/RepositoryUpdateScheduler.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/RepositoryUpdateScheduler.scala
@@ -1,19 +1,30 @@
 package com.softwaremill.codebrag.service.updater
 
 import akka.actor.{ActorRef, Props, ActorSystem}
-import scala.concurrent.duration._
 import com.softwaremill.codebrag.service.commits.{RepoDataProducer, CommitImportService}
 import com.typesafe.scalalogging.slf4j.Logging
+import com.softwaremill.codebrag.service.updater.LocalRepositoryUpdater.UpdateCommand
 
 object RepositoryUpdateScheduler extends Logging {
 
   private var actor: ActorRef = _
 
   def initialize(actorSystem: ActorSystem, repoDataProducer: RepoDataProducer, commitImportService: CommitImportService): ActorRef = {
-    import actorSystem.dispatcher
-    actor = actorSystem.actorOf(Props(new LocalRepositoryUpdater(commitImportService)), "repositoryUpdater")
-    actorSystem.scheduler.schedule(3 seconds, 45 seconds, actor, LocalRepositoryUpdater.UpdateCommand)
-    repoDataProducer.createFromConfiguration().foreach { repoData => actor ! LocalRepositoryUpdater.RefreshRepoData(repoData) }
+    actor = actorSystem.actorOf(Props(new LocalRepositoryUpdater(commitImportService, actorSystem)), "repositoryUpdater")
+    initializeRepositoryData(repoDataProducer)
+    scheduleRepositorySynchronization(actorSystem)
     actor
+  }
+
+
+  def initializeRepositoryData(repoDataProducer: RepoDataProducer) {
+    repoDataProducer.createFromConfiguration().foreach {
+      repoData => actor ! LocalRepositoryUpdater.RefreshRepoData(repoData)
+    }
+  }
+
+  def scheduleRepositorySynchronization(actorSystem: ActorSystem) {
+    import actorSystem.dispatcher
+    actorSystem.scheduler.scheduleOnce(UpdateCommand.InitialDelay, actor, LocalRepositoryUpdater.UpdateCommand)
   }
 }
