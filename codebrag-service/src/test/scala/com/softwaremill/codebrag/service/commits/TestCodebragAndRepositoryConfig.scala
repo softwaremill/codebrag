838d0dd5c93e9f1f37e5af5bff57965c590390a9
adamw <adam@warski.org>
2013-08-26 20:27:47 +0200

    SVN branch review: overriding the config that is used, instead of providing a long test config string

--- codebrag-service/src/test/scala/com/softwaremill/codebrag/service/commits/TestCodebragAndRepositoryConfig.scala
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/commits/TestCodebragAndRepositoryConfig.scala
@@ -6,6 +6,7 @@ import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
 object TestCodebragAndRepositoryConfig extends CodebragConfig with RepositoryConfig {
   override lazy val localGitStoragePath = ""
 
-  def rootConfig = ConfigFactory.parseString("repository.type=git-ssh, repository.git-ssh.name=codebrag, repository.git-ssh.uri=\"git@github.com:/softwaremill/codebrag.git\", repository.git-ssh.passphrase=abc, repository.git-ssh.branch=refs/heads/master")
+  def rootConfig = ???
 
+  override lazy val repositoryType = GitSshRepoType.configRepoType
 }
