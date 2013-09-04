6192bf30e58c4fb573b50e7ca8af50933c798e00
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-09-04 15:02:13 +0200

    Get rid of isolated scope from directive. Using isolated scope breaks impacts other directives like ng-click.

--- codebrag-ui/src/main/webapp/scripts/session/showOnDemoDirective.js
+++ codebrag-ui/src/main/webapp/scripts/session/showOnDemoDirective.js
@@ -3,7 +3,6 @@ angular.module('codebrag.session')
     .directive('showOnDemo', function(configService) {
         return {
             restrict: 'A',
-            scope: {},
             link: function(scope, el) {
                 el.hide();
                 configService.fetchConfig().then(function(config) {
