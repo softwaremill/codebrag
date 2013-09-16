89e927d5f06706683d73e60b131083b93b1c236f
Piotr Buda <pibuda@gmail.com>
2013-09-16 12:50:50 +0200

    Done a few too many things in here (check whole comment)
    - instead of waiting for synchronization, call <url>/sync to do a quick manual synchronization of repository
    - log commit messages with parameters for better output readability
    - log like messages with parameters for better output readability

--- scripts/demo/reset.sh
+++ scripts/demo/reset.sh
@@ -2,9 +2,11 @@
 
 # Variables
 
+CURL="curl -s -S"
 BASE_URL="http://localhost:8080"
 USERS_URL="$BASE_URL/rest/users"
 COMMITS_URL="$BASE_URL/rest/commits"
+SYNC_URL="$BASE_URL/sync"
 CONTENT_TYPE_HEADER="Content-type: application/json"
 ACCEPT_HEADER="Accept: application/json"
 
@@ -12,24 +14,25 @@ ACCEPT_HEADER="Accept: application/json"
 
 function sha_to_commitId {
     local sha=$1
-    echo $(mongo codebrag --eval "db.commit_infos.find({\"sha\":\"$sha\"},{\"sha\":1}).forEach(function(x){print(x._id);})" \
+    local commitId=$(mongo codebrag --eval "db.commit_infos.find({\"sha\":\"$sha\"},{\"sha\":1}).forEach(function(x){print(x._id);})" \
             | tail -c 27 | head -c 24)
+    echo $commitId
 }
 
 function comment {
     local sha=$1
-    local commitId=$(sha_to_commitId $sha)
+    local commitId=$(sha_to_commitId ${sha})
     local data=$2
-    curl -v -b cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST \
+    ${CURL} -b cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST \
         -d "$data" \
-        "$COMMITS_URL/$commitId/comments"
+        "$COMMITS_URL/$commitId/comments" > /dev/null
 }
 
 function comment_commit {
     local sha=$1
     local user=$2
     local message=$3
-
+    echo "Placing comment on commit $sha"
     comment $sha "{\"userId\":\"$user\",\"body\":\"$message\"}"
 }
 
@@ -39,6 +42,7 @@ function comment_line {
     local fileName=$3
     local lineNumber=$4
     local message=$5
+    echo "Placing comment on line $lineNumber of file $fileName in commit $sha"
 
     comment $sha "{\"userId\":\"$user\",\"body\":\"$message\",\"fileName\":\"$fileName\",\"lineNumber\":$lineNumber}"
 }
@@ -47,14 +51,16 @@ function like {
     local sha=$1
     local commitId=$(sha_to_commitId $sha)
     local data=$2
-    curl -v -b cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST \
+
+    ${CURL} -b cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST \
           -d "$data" \
-          "$COMMITS_URL/$commitId/likes"
+          "$COMMITS_URL/$commitId/likes" > /dev/null
 }
 
 function like_commit {
     local sha=$1
     local user=$2
+    echo "Liking commit $sha"
 
     like $sha "{\"userId\":\"$user\"}"
 }
@@ -64,6 +70,7 @@ function like_line {
     local user=$2
     local fileName=$3
     local lineNumber=$4
+    echo "Liking line $lineNumber of file $fileName in commit $sha"
 
     like $sha "{\"user\":\"$user\",\"fileName\":\"$fileName\",\"lineNumber\":$lineNumber}"
 }
@@ -72,34 +79,44 @@ function login {
     local user=$1
     local pass=$2
 
-    local response=$(curl -v -c cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST -d "{\"login\":\"$user\",\"password\":\"$pass\"}" $USERS_URL)
-    echo "$response" | head -c 31 | tail -c 24
+    echo $(${CURL} -c cookies.txt -H "$CONTENT_TYPE_HEADER" -H "$ACCEPT_HEADER" -X POST -d "{\"login\":\"$user\",\"password\":\"$pass\"}" ${USERS_URL} | head -c 31 | tail -c 24)
 }
 
 function logout {
-    curl -v -b cookies.txt "$USERS_URL/logout"
+    ${CURL} -b cookies.txt "$USERS_URL/logout"
+}
+
+function synchronize {
+    ${CURL} ${SYNC_URL}
 }
 
 ###############################################################################
 
 # clear everything before start
+echo "Cleaning up"
 rm cookies.txt
 mongo codebrag --eval "db.dropDatabase()"
 
 # add demo users
+echo "Importing demo users"
 mongoimport -d codebrag -c users demo_users.json
 
-# wait for next synchronization
-sleep 45
+# sync repository
+echo "Synchronizing repository"
+if [[ $(synchronize) =~ "Repository synchronized" ]]
+then
+    echo "Logging in as user 'fox'"
+    user=$(login "fox" "codebrag")
 
-user=$(login "fox" "codebrag")
+    comment_commit d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user" "Test comment"
+    comment_line d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user" "src/test/scala/com/softwaremill/gameoflife/BoardTest.scala" 57 "You could use 'should equal(...)' instead of 'should be ===' for more readability"
+    like_commit d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user"
+    like_line d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user" "src/test/scala/com/softwaremill/gameoflife/BoardTest.scala" 57
 
-comment_commit d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user" "Test comment"
-comment_line "d7ac6aadb937e6eca61df53a0016fb2c019ebe1c" "$user" "src/test/scala/com/softwaremill/gameoflife/BoardTest.scala" 57 "You could use 'should equal(...)' instead of 'should be ===' for more readability"
-like_commit d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user"
-like_line d7ac6aadb937e6eca61df53a0016fb2c019ebe1c "$user" "src/test/scala/com/softwaremill/gameoflife/BoardTest.scala" 57
-
-logout
-
+    echo "Logging fox out"
+    logout
+else
+    echo "Synchronization not successful"
+fi
 
 
