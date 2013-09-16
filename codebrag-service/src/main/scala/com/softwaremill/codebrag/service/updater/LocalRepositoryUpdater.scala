abe63a26575dab1983137bad91821826d08d3a60
Piotr Buda <pibuda@gmail.com>
2013-09-16 12:47:06 +0200

    Added sending of feedback from updated to synchronization requestors

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
@@ -4,22 +4,26 @@ import akka.actor._
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.service.commits.{RepoData, CommitImportService}
 import scala.Some
-import com.softwaremill.codebrag.service.updater.LocalRepositoryUpdater.UpdateCommand
+import com.softwaremill.codebrag.service.updater.LocalRepositoryUpdater.{RepositoryUpdateFailed, UpdateCommand}
 
 class LocalRepositoryUpdater(importService: CommitImportService, actorSystem: ActorSystem) extends Actor with Logging {
 
   private var repoData: Option[RepoData] = None
 
   def receive = {
-    case LocalRepositoryUpdater.RefreshRepoData(newRepoData)  => {
+    case LocalRepositoryUpdater.RefreshRepoData(newRepoData) => {
       repoData = Some(newRepoData)
       logger.debug("Repository credentials refreshed")
     }
     case LocalRepositoryUpdater.UpdateCommand => {
       try {
         repoData.foreach(importService.importRepoCommits)
+        sender ! LocalRepositoryUpdater.RepositoryUpdateSucceeded("Repository synchronized")
       } catch {
-        case e: Exception => logger.error("Exception while importing commits", e)
+        case e: Exception => {
+          logger.error("Exception while importing commits", e)
+          sender ! RepositoryUpdateFailed(e)
+        }
       } finally {
         import actorSystem.dispatcher
         logger.debug("Scheduling next update delay to " + UpdateCommand.NextUpdatesInterval.toString)
@@ -37,4 +41,9 @@ object LocalRepositoryUpdater {
     val NextUpdatesInterval = 45 seconds
   }
   case class RefreshRepoData(newRepoData: RepoData)
+
+  case class RepositoryUpdateSucceeded(message: String)
+
+  case class RepositoryUpdateFailed(reason: Exception)
+
 }
