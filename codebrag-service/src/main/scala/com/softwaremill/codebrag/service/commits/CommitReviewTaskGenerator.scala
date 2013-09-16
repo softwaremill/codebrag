bf1e75103e0a2b266489faab4999287634e03c3b
adamw <adam@warski.org>
2013-09-16 18:07:10 +0200

    Hopefully this will fix the build :)

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/CommitReviewTaskGenerator.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/CommitReviewTaskGenerator.scala
@@ -3,7 +3,6 @@ package com.softwaremill.codebrag.service.commits
 import com.softwaremill.codebrag.dao.{CommitInfoDAO, CommitReviewTaskDAO, UserDAO}
 import com.softwaremill.codebrag.domain.CommitsUpdatedEvent
 import akka.actor.Actor
-import pl.softwaremill.common.util.time.Clock
 import com.typesafe.scalalogging.slf4j.Logging
 
 class CommitReviewTaskGenerator(val userDao: UserDAO, val commitToReviewDao: CommitReviewTaskDAO, val commitInfoDao: CommitInfoDAO) extends Actor with Logging
