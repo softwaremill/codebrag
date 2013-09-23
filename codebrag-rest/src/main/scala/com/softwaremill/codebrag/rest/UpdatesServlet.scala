0d62c3d931779ef1a9891f4a1d53df7939614d8a
Piotr Buda <pibuda@gmail.com>
2013-09-23 16:00:12 +0200

    Added default endpoint for updates

--- codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/UpdatesServlet.scala
+++ codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/UpdatesServlet.scala
@@ -1,14 +1,19 @@
 package com.softwaremill.codebrag.rest
 
 import com.softwaremill.codebrag.service.user.Authenticator
+import org.joda.time.DateTime
 
 class UpdatesServlet(val authenticator: Authenticator) extends JsonServletWithAuthentication {
   before() {
     haltIfNotAuthenticated()
   }
 
+  get("/") {
+    UpdateNotification(new DateTime().getMillis, 0, 0)
+  }
+
   get("/", sinceLastTime) {
-    UpdateNotification(params.get("since").get.toLong, 10, 5)
+    UpdateNotification(new DateTime().getMillis, 10, 5)
   }
 
   private def sinceLastTime = params.get("since").isDefined
