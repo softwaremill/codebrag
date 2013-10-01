7eb314f2b75e973bd12276376641c43fed1200b6
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-10-01 11:28:58 +0200

    Invitation can say if it is valid

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/Invitation.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/Invitation.scala
@@ -3,4 +3,8 @@ package com.softwaremill.codebrag.domain
 import org.bson.types.ObjectId
 import org.joda.time.DateTime
 
-case class Invitation(code: String, invitationSender: ObjectId, expiryDate: DateTime = DateTime.now.plusHours(24))
+case class Invitation(code: String, invitationSender: ObjectId, expiryDate: DateTime = DateTime.now.plusHours(24)) {
+
+  def isStillValid(now: DateTime) = expiryDate.isAfter(now)
+
+}
