ba72cec3708f60d46be5308ce5e1ee1d691ca54a
Piotr Buda <pibuda@gmail.com>
2013-10-04 10:40:49 +0200

    Added configuration option that allows configuration of the time between user notification checks

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
@@ -1,8 +1,11 @@
 package com.softwaremill.codebrag.service.config
 
 import org.joda.time._
+import scala.concurrent.duration.FiniteDuration
 
 trait CodebragConfig extends ConfigWithDefault {
+
+  import TimeUnit._
 
   lazy val localGitStoragePath: String = rootConfig.getString("codebrag.local-git-storage-path")
   lazy val debugServicesLogin: String = rootConfig.getString("codebrag.debug-services-login")
@@ -11,21 +14,50 @@ trait CodebragConfig extends ConfigWithDefault {
   lazy val applicationUrl: String = rootConfig.getString("codebrag.application-url")
 
   lazy val invitationExpiryTime: ReadablePeriod = {
+    val key = "codebrag.invitation-expiry-time"
+    val pattern = getTimePattern(key, "24 H")
 
-    val expirationString = getString("codebrag.invitation-expiry-time", "24 H").trim
-    val timeUnit = expirationString.takeRight(1)
-    val amount = expirationString.dropRight(1).trim.toInt
-
-    timeUnit match {
-      case "H" => Hours.hours(amount)
-      case "M" => Minutes.minutes(amount)
-      case "D" => Days.days(amount)
-      case _ => throw new RuntimeException("Incorrect invitation expiry time " + expirationString)
+    pattern.unit match {
+      case Hours => org.joda.time.Hours.hours(pattern.time)
+      case Minutes => org.joda.time.Minutes.minutes(pattern.time)
+      case Days => org.joda.time.Days.days(pattern.time)
+      case _ => throw new RuntimeException(s"Incorrect invitation expiry time. Check $key configuration variable")
     }
   }
 
   lazy val userNotifications: Boolean = getBoolean("codebrag.user-notifications", default = true)
 
+  lazy val notificationsCheckInterval: FiniteDuration = {
+    import scala.concurrent.duration._
+    val key = "codebrag.notifications-check-interval"
+    val pattern = getTimePattern(key, "15 M")
+
+    pattern.unit match {
+      case Hours => pattern.time.hours
+      case Minutes => pattern.time.minutes
+      case Seconds => pattern.time.seconds
+      case _ => throw new RuntimeException(s"Incorrect invitation expiry time. Check $key configuration variable")
+    }
+  }
+
+  private def getTimePattern(key: String, default: String): TimePattern = {
+    val expirationString = getString(key, default).trim
+    val timeUnit = expirationString.takeRight(1)
+    val amount = expirationString.dropRight(1).trim.toInt
+    TimePattern(amount, TimeUnit.withName(timeUnit))
+  }
+
+  private case class TimePattern(time: Int, unit: TimeUnit)
+
+}
+
+private object TimeUnit extends Enumeration {
+  type TimeUnit = Value
+
+  val Days = Value("D")
+  val Hours = Value("H")
+  val Minutes = Value("M")
+  val Seconds = Value("S")
 }
 
 
