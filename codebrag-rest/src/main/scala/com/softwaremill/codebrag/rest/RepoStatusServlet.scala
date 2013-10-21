c8924dda057d5b82220208b60c6a9a672d0c7e5a
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-10-21 09:47:52 +0200

    Expose repository status via JSON to client

---
+++ codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/RepoStatusServlet.scala
@@ -0,0 +1,30 @@
+package com.softwaremill.codebrag.rest
+
+import com.typesafe.scalalogging.slf4j.Logging
+import com.softwaremill.codebrag.service.config.{RepositoryConfig, CodebragConfig}
+import com.softwaremill.codebrag.dao.{RepositoryHeadStore, UserDAO}
+import org.bson.types.ObjectId
+import com.softwaremill.codebrag.service.user.Authenticator
+import com.softwaremill.codebrag.service.commits.RepoDataProducer
+import org.scalatra.BadRequest
+import com.softwaremill.codebrag.domain.RepositoryStatus
+
+class RepoStatusServlet(val authenticator: Authenticator, repoDataProducer: RepoDataProducer, repoHeadStore: RepositoryHeadStore) extends JsonServletWithAuthentication with Logging {
+
+  get("/") {
+    repoDataProducer.createFromConfiguration() match {
+      case Some(repoData) => {
+        repoHeadStore.getRepoStatus(repoData.repositoryName) match {
+          case Some(status) => Map("repoStatus" -> status)
+          case None => Map("repoStatus" -> RepositoryStatus.notReady(repoData.repositoryName))
+        }
+      }
+      case None => BadRequest("Cannot get repository information from configuration")
+    }
+  }
+
+}
+
+object RepoStatusServlet {
+  val Mapping = "repoStatus"
+}
