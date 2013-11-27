b5feae5a68b7b440f5596dd7fca194978b5604a0
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-11-27 10:26:20 +0100

    Saving status fades out after 2 seconds

--- codebrag-ui/app/scripts/common/directives/savingStatus.js
+++ codebrag-ui/app/scripts/common/directives/savingStatus.js
@@ -4,7 +4,7 @@
  */
 angular.module('codebrag.common.directives')
 
-    .directive('savingStatus', function() {
+    .directive('savingStatus', function($timeout) {
 
         var statusToText = {
             "pending": 'Saving...',
@@ -15,16 +15,34 @@ angular.module('codebrag.common.directives')
             }
         };
 
+        var hideTimerHandler;
+
         return {
             restrict: 'A',
             link: function(scope, el, attrs) {
+
                 scope.$watch(attrs.savingStatus, function(changedStatus) {
                     if(changedStatus) {
-                        removeStatusClasses();
-                        el.addClass(changedStatus);
-                        el.text(statusToText[changedStatus])
+                        showStatusInfo(changedStatus);
+                        scheduleStatusInfoFadeOut(changedStatus);
                     }
                 });
+
+                function showStatusInfo(changedStatus) {
+                    $timeout.cancel(hideTimerHandler);
+                    removeStatusClasses();
+                    el.addClass(changedStatus);
+                    el.text(statusToText[changedStatus]);
+                    el.show();
+                }
+
+                function scheduleStatusInfoFadeOut(status) {
+                    if(status !== 'pending') {
+                        hideTimerHandler = $timeout(function() {
+                            el.fadeOut();
+                        }, 2000);
+                    }
+                }
 
                 function removeStatusClasses() {
                     statusToText.allClasses().forEach(function(clazz) {
