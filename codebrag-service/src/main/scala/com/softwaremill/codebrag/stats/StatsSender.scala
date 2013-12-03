94edf9ff55243f458a070626f17abdec288a6811
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-12-03 11:08:03 +0100

    Set default stats server address as https

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/stats/StatsSender.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/stats/StatsSender.scala
@@ -7,7 +7,7 @@ import dispatch._
 object StatsSender extends Logging {
 
   def sendHttpStatsRequest(jsonData: String, config: CodebragStatsConfig) {
-    val svc = dispatch.url(config.statsServerUrl).setMethod("POST").setHeader("Content-Type", "application/json") << jsonData
+    val svc = dispatch.url(config.statsServerUrl).secure.setMethod("POST").setHeader("Content-Type", "application/json") << jsonData
     try {
       Http(svc)()
     } catch {
