c5aadf9357fbf6a2bc1f172bff8a8c9ed887609b
Michal Ostruszka <michal.ostruszka@gmail.com>
2013-12-03 14:08:39 +0100

    Change cert file names

--- stats-server/lib/security.js
+++ stats-server/lib/security.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var certDir = __dirname + '/../cert/';
 
 module.exports.credentials = {
-  key: fs.readFileSync(certDir + 'localhost.key').toString(),
-  cert: fs.readFileSync(certDir + 'localhost.cert').toString()
+  key: fs.readFileSync(certDir + 'stats.codebrag.com.key').toString(),
+  cert: fs.readFileSync(certDir + 'stats.codebrag.com.cert').toString()
 };
