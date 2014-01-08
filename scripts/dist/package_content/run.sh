b0d2ab04b2ed72f801aff1bfab34436675d66925
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-01-08 08:17:20 +0100

    Set default encoding for java process to UTF-8
    Windows sucks with processing files that have UTF-8 chars and displays crap if encoding is not forced to UTF-8 explicitly

--- scripts/dist/package_content/run.sh
+++ scripts/dist/package_content/run.sh
@@ -3,5 +3,5 @@
 CONFIG=${1:-"codebrag.conf"}
 
 echo "Starting Codebrag... (with config:$CONFIG)"
-nohup java -Dconfig.file=./$CONFIG -Dlogback.configurationFile=./logback.xml -jar codebrag.jar &
+nohup java -Dfile.encoding=UTF-8 -Dconfig.file=./$CONFIG -Dlogback.configurationFile=./logback.xml -jar codebrag.jar &
 echo "Codebrag started. Logs are written to codebrag.log"
