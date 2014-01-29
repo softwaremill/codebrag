36702a42a1f624ec8e08bbcc0c49bda5c53de03c
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-01-29 13:21:07 +0100

    Add ability to deploy preview packages to S3
    They are not public (link is not visible publicly) so it can serve as preview packages for testing.
    It is auto-deployed and replaced after every build on staging env.

--- scripts/dist/dist.sh
+++ scripts/dist/dist.sh
@@ -4,13 +4,15 @@ if [ $# -eq 1 ] && [ $1 = "--help" ]; then
   echo "Prepares and optionally uploads codebrag distribution to S3"
   echo "Usage:"
   echo "dist.sh - packages codebrag distribution as zip file (codebrag.zip)"
-  echo "dist.sh upload - packages and uploads packaged to S3 storage"
+  echo "dist.sh upload - packages and uploads package to S3 storage"
+  echo "dist.sh upload-preview - packages and uploads preview package to S3 storage"
   exit
 fi
 
 CODEBRAG_ROOT=../..
 DIST_DIR=codebrag
 DIST_ARCHIVE=codebrag.zip
+DIST_ARCHIVE_PREVIEW=codebrag-preview.zip
 WAR_FILE=$CODEBRAG_ROOT/codebrag-dist/target/scala-2.10/codebrag-dist-assembly*.jar
 CONF_FILE=$CODEBRAG_ROOT/codebrag-rest/src/main/resources/application.conf.template
 DB_MIGRATIONS=$CODEBRAG_ROOT/scripts/mongo_migrations
@@ -23,6 +25,7 @@ STOP_SCRIPT=$CODEBRAG_ROOT/scripts/dist/package_content/stop.*
 PASSWD_SCRIPT=$CODEBRAG_ROOT/scripts/dist/package_content/passwd.*
 
 UPLOAD_ARG="upload"
+UPLOAD_PREVIEW_ARG="upload-preview"
 S3CMD_PATH="./lib/s3cmd-1.5.0-alpha1/s3cmd"
 S3CMD_CONFIG="./lib/s3cfg-codebrag"
 
@@ -65,18 +68,25 @@ zip -r $DIST_ARCHIVE $DIST_DIR
 echo "Done"
 
 # Upload build to S3 if required
-if [ -f $DIST_ARCHIVE ] && [ $# -eq 1 ] && [ $1 = $UPLOAD_ARG ]; then
-  DATE_DAY=`date +"%Y-%m-%d"`
+if [ -f $DIST_ARCHIVE ] && [ $# -eq 1 ]; then
+  if [ $1 = $UPLOAD_ARG ]; then
+    DATE_DAY=`date +"%Y-%m-%d"`
 
-  TARGET_PATH="s3://codebrag-dist/$DATE_DAY/$DIST_ARCHIVE"
-  echo "Uploading package to $TARGET_PATH"
-  CMD="$S3CMD_PATH -c $S3CMD_CONFIG put $DIST_ARCHIVE $TARGET_PATH"
-  `$CMD > s3upload.log`
+    TARGET_PATH="s3://codebrag-dist/$DATE_DAY/$DIST_ARCHIVE"
+    echo "Uploading package to $TARGET_PATH"
+    CMD="$S3CMD_PATH -c $S3CMD_CONFIG put $DIST_ARCHIVE $TARGET_PATH"
+    `$CMD > s3upload.log`
 
-  LATEST_PATH="s3://codebrag-dist/latest/$DIST_ARCHIVE"
-  echo "Uploading package to $LATEST_PATH"
-  CMD="$S3CMD_PATH -c $S3CMD_CONFIG put --acl-public $DIST_ARCHIVE $LATEST_PATH"
-  `$CMD >> s3upload.log`
+    LATEST_PATH="s3://codebrag-dist/latest/$DIST_ARCHIVE"
+    echo "Uploading package to $LATEST_PATH"
+    CMD="$S3CMD_PATH -c $S3CMD_CONFIG put --acl-public $DIST_ARCHIVE $LATEST_PATH"
+    `$CMD >> s3upload.log`
+  elif [ $1 = $UPLOAD_PREVIEW_ARG ]; then
+    PREVIEW_PATH="s3://codebrag-dist/preview/$DIST_ARCHIVE_PREVIEW"
+    echo "Uploading package to $PREVIEW_PATH"
+    CMD="$S3CMD_PATH -c $S3CMD_CONFIG put --acl-public $DIST_ARCHIVE $PREVIEW_PATH"
+    `$CMD >> s3upload.log`
+  fi
   echo "Done"
 fi
 
