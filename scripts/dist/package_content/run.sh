404941855974dff8709efb4943a7a9c238b753fa
pawelw <pawel@softwaremill.pl>
2014-01-29 15:40:26 +0100

    JDK, JRE - doesn't matter

--- scripts/dist/package_content/run.sh
+++ scripts/dist/package_content/run.sh
@@ -4,7 +4,7 @@ function java_not_installed {
   local JAVA_VERSION_REQUIRED=7
   local JAVA_VERSION_CURRENT=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0,numbers,"."); print numbers[2]}'`
   if [ $JAVA_VERSION_CURRENT -lt $JAVA_VERSION_REQUIRED ]; then
-    echo "ERROR: Java JDK in required version $JAVA_VERSION_REQUIRED not found" >&2
+    echo "ERROR: Java in required version $JAVA_VERSION_REQUIRED not found" >&2
     echo 0
   else
     echo 1
