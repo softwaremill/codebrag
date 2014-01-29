b2768ad7fa1fb4aa6bb138349f6bf64fcc7dbfaf
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-01-29 15:54:57 +0100

    When no java found alert user too

--- scripts/dist/package_content/run.sh
+++ scripts/dist/package_content/run.sh
@@ -3,7 +3,7 @@
 function java_not_installed {
   local JAVA_VERSION_REQUIRED=7
   local JAVA_VERSION_CURRENT=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0,numbers,"."); print numbers[2]}'`
-  if [ $JAVA_VERSION_CURRENT -lt $JAVA_VERSION_REQUIRED ]; then
+  if [[ -z $JAVA_VERSION_CURRENT || $JAVA_VERSION_CURRENT -lt $JAVA_VERSION_REQUIRED ]]; then
     echo "ERROR: Java in required version $JAVA_VERSION_REQUIRED not found" >&2
     echo 0
   else
@@ -21,5 +21,3 @@ CONFIG=${1:-"codebrag.conf"}
 echo "Starting Codebrag... (with config:$CONFIG)"
 nohup java -Dfile.encoding=UTF-8 -Dconfig.file=./$CONFIG -Dlogback.configurationFile=./logback.xml -jar codebrag.jar &
 echo "Codebrag started. Logs are written to codebrag.log"
-
-
