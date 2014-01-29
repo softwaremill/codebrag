72677623914657a8613331b54741666b2b936a7c
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-01-29 12:53:09 +0100

    Added pre-run checks block to check for required things. For now only java version >= 1.7 check is available

--- scripts/dist/package_content/run.sh
+++ scripts/dist/package_content/run.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
+function java_not_installed {
+  echo "test"
+  local JAVA_VERSION_REQUIRED=1.8*
+  local JAVA_VERSION_CURRENT=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \"`
+  if ! [[ $JAVA_VERSION_CURRENT == $JAVA_VERSION_REQUIRED ]]; then
+    echo "ERROR: Java JDK in required version $JAVA_VERSION_REQUIRED not found" >&2
+    echo 0
+  else
+    echo 1
+  fi
+}
+
+JAVA=$(java_not_installed)
+
+if [[ JAVA ]]; then
+  echo "Please install required version of Java JDK first"
+  exit 1
+fi
+
 CONFIG=${1:-"codebrag.conf"}
 
 echo "Starting Codebrag... (with config:$CONFIG)"
 nohup java -Dfile.encoding=UTF-8 -Dconfig.file=./$CONFIG -Dlogback.configurationFile=./logback.xml -jar codebrag.jar &
 echo "Codebrag started. Logs are written to codebrag.log"
+
+
