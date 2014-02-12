17e6bd0e601d878f7e6cfd0cf040261dc54008c1
adamw <adam@warski.org>
2014-02-12 15:08:00 +0100

    [refactor] Extracting a class with user schema

--- codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/user/SQLUserDAO.scala
+++ codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/user/SQLUserDAO.scala
@@ -4,10 +4,8 @@ import com.softwaremill.codebrag.domain._
 import org.bson.types.ObjectId
 import com.softwaremill.codebrag.dao.sql.{WithSQLSchemas, SQLDatabase}
 import scala.slick.driver.JdbcProfile
-import org.joda.time.DateTime
-import scala.slick.model.ForeignKeyAction
 
-class SQLUserDAO(database: SQLDatabase) extends UserDAO with WithSQLSchemas {
+class SQLUserDAO(val database: SQLDatabase) extends UserDAO with WithSQLSchemas with SQLUserSchema {
   import database.driver.simple._
   import database._
 
@@ -85,84 +83,6 @@ class SQLUserDAO(database: SQLDatabase) extends UserDAO with WithSQLSchemas {
     } yield (u.name, u.emailLowerCase, s.avatarUrl)
 
     q.list().map(PartialUserDetails.tupled)
-  }
-
-  private case class SQLAuth(id: ObjectId, provider: String, username: String, usernameLowerCase: String, token: String, salt: String) {
-    def toAuth = Authentication(provider, username, usernameLowerCase, token, salt)
-  }
-  private def toSQLAuth(id: ObjectId, auth: Authentication) = SQLAuth(id, auth.provider, auth.username, auth.usernameLowerCase,
-    auth.token, auth.salt)
-
-  private case class SQLSettings(id: ObjectId, avatarUrl: String, emailNotificationsEnabled: Boolean,
-    dailyUpdatesEmailEnabled: Boolean, appTourDone: Boolean) {
-    def toSettings = UserSettings(avatarUrl, emailNotificationsEnabled, dailyUpdatesEmailEnabled, appTourDone)
-  }
-  private def toSQLSettings(id: ObjectId, settings: UserSettings) = SQLSettings(id, settings.avatarUrl,
-    settings.emailNotificationsEnabled, settings.dailyUpdatesEmailEnabled, settings.appTourDone)
-
-  private case class SQLLastNotif(id: ObjectId, commits: Option[DateTime], followups: Option[DateTime]) {
-    def toLastNotif = LastUserNotificationDispatch(commits, followups)
-  }
-  private def toSQLLastNotif(id: ObjectId, lastNotif: LastUserNotificationDispatch) = SQLLastNotif(id,
-    lastNotif.commits, lastNotif.followups)
-
-  private type UserTuple = (ObjectId, String, String, String)
-
-  private class Auths(tag: Tag) extends Table[SQLAuth](tag, "users_authentications") {
-    def userId = column[ObjectId]("user_id", O.PrimaryKey)
-    def provider = column[String]("provider")
-    def username = column[String]("username")
-    def usernameLowerCase = column[String]("username_lowercase")
-    def token = column[String]("token")
-    def salt = column[String]("salt")
-
-    def * = (userId, provider, username, usernameLowerCase, token, salt) <> (SQLAuth.tupled, SQLAuth.unapply)
-  }
-
-  private val auths = TableQuery[Auths]
-  
-  private class Settings(tag: Tag) extends Table[SQLSettings](tag, "users_settings") {
-    def userId = column[ObjectId]("user_id", O.PrimaryKey)
-    def avatarUrl = column[String]("avatar_url")
-    def emailNotificationsEnabled = column[Boolean]("email_notif")
-    def dailyUpdatesEmailEnabled = column[Boolean]("email_daily_updates")
-    def appTourDone = column[Boolean]("app_tour_done")
-
-    def * = (userId, avatarUrl, emailNotificationsEnabled, dailyUpdatesEmailEnabled, appTourDone) <> (SQLSettings.tupled, SQLSettings.unapply)
-  }
-
-  private val userSettings = TableQuery[Settings]
-
-  private class LastNotifs(tag: Tag) extends Table[SQLLastNotif](tag, "users_last_notifs") {
-    def userId = column[ObjectId]("user_id", O.PrimaryKey)
-    def lastCommitsDispatch = column[Option[DateTime]]("last_commits_dispatch")
-    def lastFollowupsDispatch = column[Option[DateTime]]("last_followups_dispatch")
-
-    def * = (userId, lastCommitsDispatch, lastFollowupsDispatch) <> (SQLLastNotif.tupled, SQLLastNotif.unapply)
-  }
-
-  private val lastNotifs = TableQuery[LastNotifs]
-
-  private class Users(tag: Tag) extends Table[UserTuple](tag, "users") {
-    def id = column[ObjectId]("id", O.PrimaryKey)
-    def name = column[String]("name")
-    def emailLowerCase = column[String]("email_lowercase")
-    def token = column[String]("token")
-
-    def auth = foreignKey("AUTH_FK", id, auths)(_.userId, ForeignKeyAction.Cascade, ForeignKeyAction.Cascade)
-    def settings = foreignKey("SETTINGS_FK", id, userSettings)(_.userId, ForeignKeyAction.Cascade, ForeignKeyAction.Cascade)
-    def lastNotif = foreignKey("LAST_NOTIFS_FK", id, lastNotifs)(_.userId, ForeignKeyAction.Cascade, ForeignKeyAction.Cascade)
-
-    def * = (id, name, emailLowerCase, token)
-  }
-
-  private val users = TableQuery[Users]
-
-  private def tuple(user: User): UserTuple = (user.id, user.name, user.emailLowerCase, user.token)
-
-  private val untuple: ((UserTuple, SQLAuth, SQLSettings, SQLLastNotif)) => User = {
-    case (tuple, sqlAuth, sqlSettings, sqlLastNotif) =>
-      User(tuple._1, sqlAuth.toAuth, tuple._2, tuple._3, tuple._4, sqlSettings.toSettings, sqlLastNotif.toLastNotif)
   }
 
   private def findOneWhere(condition: Users => Column[Boolean]): Option[User] = db.withTransaction { implicit session =>
