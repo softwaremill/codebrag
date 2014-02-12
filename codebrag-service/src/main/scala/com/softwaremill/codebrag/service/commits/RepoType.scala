aa4248ead9629bee41ce824474398e66ea9b19ec
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-02-12 15:16:58 +0100

    Cleanup old repo configuration related classes

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/RepoType.scala
+++
@@ -1,8 +0,0 @@
-package com.softwaremill.codebrag.service.commits
-
-sealed abstract class RepoType(val configRepoType: String)
-
-case object GithubRepoType extends RepoType("github")
-case object GitHttpsRepoType extends RepoType("git-https")
-case object GitSshRepoType extends RepoType("git-ssh")
-case object SvnRepoType extends RepoType("svn")
