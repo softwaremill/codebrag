0e4b03b485dcf691b0368eba20c70cf717dacb70
adamw <adam@warski.org>
2014-02-12 15:13:16 +0100

    [refactor] Extracting a class with commit info schema

--- codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/commitinfo/SQLCommitInfoDAO.scala
+++ codebrag-dao/src/main/scala/com/softwaremill/codebrag/dao/commitinfo/SQLCommitInfoDAO.scala
@@ -2,11 +2,11 @@ package com.softwaremill.codebrag.dao.commitinfo
 
 import com.softwaremill.codebrag.dao.sql.{WithSQLSchemas, SQLDatabase}
 import org.joda.time.DateTime
-import com.softwaremill.codebrag.domain.{CommitFileInfo, CommitInfo, UserLike}
+import com.softwaremill.codebrag.domain.{CommitInfo, UserLike}
 import org.bson.types.ObjectId
 import scala.slick.driver.JdbcProfile
 
-class SQLCommitInfoDAO(database: SQLDatabase) extends CommitInfoDAO with WithSQLSchemas {
+class SQLCommitInfoDAO(val database: SQLDatabase) extends CommitInfoDAO with WithSQLSchemas with SQLCommitInfoSchema {
   import database.driver.simple._
   import database._
 
@@ -73,63 +73,6 @@ class SQLCommitInfoDAO(database: SQLDatabase) extends CommitInfoDAO with WithSQL
       .list()
       .map(_.toPartialCommitDetails)
   }
-
-  private case class SQLCommitInfo(
-    id: ObjectId, sha: String, message: String, authorName: String, authorEmail: String,
-    committerName: String, committerEmail: String, authorDate: DateTime, commitDate: DateTime) {
-
-    def toCommitInfo(sfs: List[SQLCommitInfoFile], sps: List[SQLCommitInfoParent]) = CommitInfo(
-      id, sha, message, authorName, authorEmail, committerName, committerEmail, authorDate, commitDate,
-      sps.map(_.parent), sfs.map(f => CommitFileInfo(f.filename, f.status, f.patch))
-    )
-
-    def toPartialCommitDetails = PartialCommitInfo(id, sha, message, authorName, authorEmail, authorDate.toDate)
-  }
-
-  private case class SQLCommitInfoFile(commitId: ObjectId, filename: String, status: String, patch: String)
-
-  private case class SQLCommitInfoParent(commitId: ObjectId, parent: String)
-
-  private class CommitInfos(tag: Tag) extends Table[SQLCommitInfo](tag, "commit_infos") {
-    def id = column[ObjectId]("id", O.PrimaryKey)
-    def sha = column[String]("sha")
-    def message = column[String]("message")
-    def authorName = column[String]("author_name")
-    def authorEmail = column[String]("author_email")
-    def commiterName = column[String]("commiter_name")
-    def commiterEmail = column[String]("commiter_email")
-    def authorDate = column[DateTime]("author_date")
-    def commitDate = column[DateTime]("commit_date")
-
-    def * = (id, sha, message, authorName, authorEmail, commiterName, commiterEmail, authorDate, commitDate) <>
-      (SQLCommitInfo.tupled, SQLCommitInfo.unapply)
-  }
-
-  private val commitInfos = TableQuery[CommitInfos]
-
-  private class CommitInfosFiles(tag: Tag) extends Table[SQLCommitInfoFile](tag, "commit_infos_files") {
-    def commitInfoId = column[ObjectId]("commit_info_id")
-    def filename = column[String]("filename")
-    def status = column[String]("status")
-    def patch = column[String]("patch")
-
-    def commitInfo = foreignKey("COMMIT_INFO_FILES_FK", commitInfoId, commitInfos)(_.id)
-
-    def * = (commitInfoId, filename, status, patch) <> (SQLCommitInfoFile.tupled, SQLCommitInfoFile.unapply)
-  }
-
-  private val commitInfosFiles = TableQuery[CommitInfosFiles]
-
-  private class CommitInfosParents(tag: Tag) extends Table[SQLCommitInfoParent](tag, "commit_infos_parents") {
-    def commitInfoId = column[ObjectId]("commit_info_id")
-    def parent = column[String]("parent")
-
-    def commitInfo = foreignKey("COMMIT_INFO_PARENTS_FK", commitInfoId, commitInfos)(_.id)
-
-    def * = (commitInfoId, parent) <> (SQLCommitInfoParent.tupled, SQLCommitInfoParent.unapply)
-  }
-
-  private val commitInfosParents = TableQuery[CommitInfosParents]
 
   private def findOneWhere(condition: CommitInfos => Column[Boolean]): Option[CommitInfo] = db.withTransaction { implicit session =>
     val q = for {
