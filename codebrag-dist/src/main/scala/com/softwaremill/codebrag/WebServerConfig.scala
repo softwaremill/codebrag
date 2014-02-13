91717f7f0ccf083255130ba786acd4b9072409f0
adamw <adam@warski.org>
2014-02-13 09:07:09 +0100

    How did this end up being renamed?

--- codebrag-dist/src/main/scala/com/softwaremill/codebrag/WebServerConfig.scala
+++ codebrag-dist/src/main/scala/com/softwaremill/codebrag/WebServerConfig.scala
@@ -3,7 +3,7 @@ package com.softwaremill.codebrag
 import com.typesafe.config.Config
 
 trait WebServerConfig {
-  def rootConfig: AllConfig
+  def rootConfig: Config
 
   lazy val webServerHost: String = rootConfig.getString("web-server.host")
   lazy val webServerPort: Int = rootConfig.getInt("web-server.port")
