bf625b8fe6c883688b820fff8a29921a8f30f7d5
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-02-14 12:26:06 +0100

    Remove old settings for repository path

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
@@ -5,7 +5,6 @@ import scala.concurrent.duration._
 
 trait CodebragConfig extends ConfigWithDefault with CodebragStatsConfig {
 
-  lazy val localGitStoragePath: String = rootConfig.getString("codebrag.local-git-storage-path")
   lazy val debugServicesLogin: String = rootConfig.getString("codebrag.debug-services-login")
   lazy val debugServicesPassword: String = rootConfig.getString("codebrag.debug-services-password")
   lazy val demo: Boolean = getBoolean("codebrag.demo", default = false)
