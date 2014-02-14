2bf8600f5b69d10b398d3342db12346be5b5a5db
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-02-14 15:38:24 +0100

    Doc describing how repository section may look like in config

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/RepositoryConfig.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/RepositoryConfig.scala
@@ -2,12 +2,26 @@ package com.softwaremill.codebrag.service.config
 
 import com.typesafe.config.Config
 
+/*
+  This is repository section from config file. It looks like below:
+  It can have either username/password keys or passphrase key configured.
+  Also it can have repositories directory set. By default it is set to "./repos"
+
+  repository {
+    username = "johndoe"
+    password = "secret"
+
+    repos-root = "/your/repos/root"
+  }
+
+ */
+
 trait RepositoryConfig extends ConfigWithDefault {
 
   def rootConfig: Config
 
   // directory containing cloned repo dir
-  val repositoriesRoot = getString("repository.repos-root", "repos")
+  val repositoriesRoot = getString("repository.repos-root", "./repos")
 
   // for user/pass
   val username = getOptional("repository.username")
