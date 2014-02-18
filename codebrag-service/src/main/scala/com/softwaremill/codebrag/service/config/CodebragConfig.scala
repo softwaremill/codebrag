229d9398a969ad883c2c1590bc9a734aef508585
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-02-18 15:13:25 +0100

    Extract notification config to separate trait

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/CodebragConfig.scala
@@ -4,32 +4,30 @@ import org.joda.time._
 import scala.concurrent.duration._
 import com.softwaremill.codebrag.common.config.ConfigWithDefault
 
-trait CodebragConfig extends ConfigWithDefault with CodebragStatsConfig {
+trait CodebragConfig extends ConfigWithDefault with StatsConfig with EmailNotificationConfig {
 
-  lazy val debugServicesLogin: String = rootConfig.getString("codebrag.debug-services-login")
-  lazy val debugServicesPassword: String = rootConfig.getString("codebrag.debug-services-password")
   lazy val demo: Boolean = getBoolean("codebrag.demo", default = false)
   lazy val applicationUrl: String = rootConfig.getString("codebrag.application-url")
 
   lazy val invitationExpiryTime: ReadablePeriod = Period.millis(getMilliseconds("codebrag.invitation-expiry-time", 24.hours.toMillis).toInt)
 
-  lazy val userNotifications: Boolean = getBoolean("codebrag.user-email-notifications.enabled", default = true)
+  lazy val replayFollowupsForPastCommitsTimeInDays = getInt("codebrag.replay-followups-time", 30)
+}
 
-  lazy val notificationsCheckInterval = getMilliseconds("codebrag.user-email-notifications.check-interval", 15.minutes.toMillis).millis
-
-  lazy val userOfflinePeriod = Period.millis(getMilliseconds("codebrag.user-email-notifications.user-offline-after", 5.minutes.toMillis).toInt)
+trait EmailNotificationConfig extends ConfigWithDefault {
+  lazy val userNotifications: Boolean = getBoolean("email-notifications.enabled", default = true)
+  lazy val notificationsCheckInterval = getMilliseconds("email-notifications.check-interval", 15.minutes.toMillis).millis
+  lazy val userOfflinePeriod = Period.millis(getMilliseconds("email-notifications.user-offline-after", 5.minutes.toMillis).toInt)
 
   // Config properties for daily summary send outs
   // Not included in docs and in config template as we prefer not to expose them
   // By default daily summaries are sent at 6am every day
-  lazy val dailyDigestSendHour = getInt("codebrag.user-email-notifications.daily-digest-hour", 6)
-  lazy val dailyDigestSendMinute = getInt("codebrag.user-email-notifications.daily-digest-minute", 0)
-  lazy val dailyDigestSendInterval = Period.millis(getMilliseconds("codebrag.user-email-notifications.daily-digest-interval", 24.hours.toMillis).toInt)
-
-  lazy val replayFollowupsForPastCommitsTimeInDays = getInt("codebrag.replay-followups-time", 30)
+  lazy val dailyDigestSendHour = getInt("email-notifications.daily-digest-hour", 6)
+  lazy val dailyDigestSendMinute = getInt("email-notifications.daily-digest-minute", 0)
+  lazy val dailyDigestSendInterval = Period.millis(getMilliseconds("email-notifications.daily-digest-interval", 24.hours.toMillis).toInt)
 }
 
-trait CodebragStatsConfig extends ConfigWithDefault {
+trait StatsConfig extends ConfigWithDefault {
 
   lazy val sendStats = getBoolean("codebrag.send-anon-usage-data", true)
   lazy val statsSendHour = getInt("codebrag.stats-send-hour", 3)
