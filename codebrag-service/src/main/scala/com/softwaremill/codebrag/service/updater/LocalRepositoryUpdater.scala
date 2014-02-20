93294f485ec3af8b2dee265cf0fdb20fbd57e194
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-02-20 14:01:57 +0100

    Cleanup - remove not used events

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/updater/LocalRepositoryUpdater.scala
@@ -3,7 +3,6 @@ package com.softwaremill.codebrag.service.updater
 import akka.actor._
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.service.commits.CommitImportService
-import com.softwaremill.codebrag.service.updater.LocalRepositoryUpdater.RepositoryUpdateFailed
 import com.softwaremill.codebrag.repository.config.RepoData
 
 class LocalRepositoryUpdater(importService: CommitImportService, repoData: RepoData, actorSystem: ActorSystem) extends Actor with Logging {
@@ -12,11 +11,9 @@ class LocalRepositoryUpdater(importService: CommitImportService, repoData: RepoD
     case LocalRepositoryUpdater.UpdateCommand(schedule) => {
       try {
         importService.importRepoCommits(repoData)
-        sender ! LocalRepositoryUpdater.RepositoryUpdateSucceeded("Repository synchronized")
       } catch {
         case e: Exception => {
           logger.error("Exception while importing commits", e)
-          sender ! RepositoryUpdateFailed(e)
         }
       } finally {
         if (schedule) {
@@ -37,9 +34,5 @@ object LocalRepositoryUpdater {
   val NextUpdatesInterval = 45 seconds
 
   case class UpdateCommand(scheduleRecurring: Boolean)
-
-  case class RepositoryUpdateSucceeded(message: String)
-
-  case class RepositoryUpdateFailed(reason: Exception)
 
 }
