7c4bd0ec452d4710fa7876cf6253419f1d8ce57a
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-04 13:31:04 +0100

    Transport classes for multibranch commits loading

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/LoadCommitsResult.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/LoadCommitsResult.scala
@@ -1,3 +1,6 @@
 package com.softwaremill.codebrag.domain
 
 case class LoadCommitsResult(commits: List[CommitInfo], repoName: String, currentRepoHeadSHA: String)
+
+case class MultibranchLoadCommitsResult(repoName: String, commits: List[CommitsForBranch])
+case class CommitsForBranch(branchName: String, commits: List[CommitInfo], currentBranchSHA: String)
