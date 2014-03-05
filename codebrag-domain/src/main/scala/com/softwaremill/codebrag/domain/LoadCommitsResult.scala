db4a7b116bcd211749c6f27e4404314ac9d934fb
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-05 12:39:09 +0100

    Commits for given branch are lightweight commits representations

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/LoadCommitsResult.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/LoadCommitsResult.scala
@@ -3,4 +3,4 @@ package com.softwaremill.codebrag.domain
 case class LoadCommitsResult(commits: List[CommitInfo], repoName: String, currentRepoHeadSHA: String)
 
 case class MultibranchLoadCommitsResult(repoName: String, commits: List[CommitsForBranch])
-case class CommitsForBranch(branchName: String, commits: List[CommitInfo], currentBranchSHA: String)
+case class CommitsForBranch(branchName: String, commits: List[PartialCommitInfo], currentBranchSHA: String)
