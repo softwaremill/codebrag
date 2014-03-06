b4264097fc273befb0d68d838b584550734c3266
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-06 14:10:05 +0100

    PartialCommitInfo can convert itself to CommitInfo
    This is temp conversion to avoid dealing with Slick for now. Will be removed as soon as DAO gets changed

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/PartialCommitInfo.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/PartialCommitInfo.scala
@@ -3,7 +3,13 @@ package com.softwaremill.codebrag.domain
 import org.bson.types.ObjectId
 import org.joda.time.DateTime
 
-case class PartialCommitInfo(id: ObjectId, sha: String, message: String, authorName: String, authorEmail: String, date: DateTime)
+case class PartialCommitInfo(id: ObjectId, sha: String, message: String, authorName: String, authorEmail: String, date: DateTime) {
+
+  // temp method to convert to full commit info to be able to persist it
+  // TODO: remove that and change Slick persistence to take PartialCommitInfo
+  def toCommitInfo = CommitInfo(sha, message, authorName, authorEmail, authorName, authorEmail, date, date, List.empty[String])
+
+}
 
 object PartialCommitInfo {
 
