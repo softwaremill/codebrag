ea777839049db006df46aaf2219024e9b092ae0b
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-06 13:22:08 +0100

    WIP Initial implementation of im-memory cache for commits per branch

---
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/branches/RepositoryCache.scala
@@ -0,0 +1,57 @@
+package com.softwaremill.codebrag.service.commits.branches
+
+import com.typesafe.scalalogging.slf4j.Logging
+import com.softwaremill.codebrag.repository.Repository
+import com.softwaremill.codebrag.domain.MultibranchLoadCommitsResult
+
+
+/**
+ * Keeps commits (SHA) for all repository branches
+ */
+class RepositoryCache extends Logging {
+
+  private val MaxCommitsPerBranchCount = 10
+
+  // TODO: probably change this
+  private val commits = new scala.collection.mutable.HashMap[String, List[String]]
+
+  def addCommits(loadResult: MultibranchLoadCommitsResult) {
+    loadResult.commits.foreach { branchCommits =>
+      val commitsShas = branchCommits.commits.map(_.sha)
+      addCommitsToBranch(commitsShas, branchCommits.branchName)
+    }
+  }
+
+  private def addCommitsToBranch(newCommits: List[String], branchName: String) {
+    logger.debug(s"Adding ${newCommits.size} to ${branchName}")
+    val finalCommits = commits.get(branchName) match {
+      case Some(commits) => newCommits ::: commits
+      case None => newCommits
+    }
+    // TODO: cut from the bottom if exceeds max limit
+    commits.put(branchName, finalCommits)
+    logger.debug(s"Final number of commits in ${branchName}: ${commits.get(branchName).getOrElse(List.empty).size}")
+  }
+
+  def getCommitsForBranchExcluding(commitsToExclude: List[String], branchName: String): List[String] = {
+    logger.debug(s"Getting branch commits for ${branchName} and excluding ${commitsToExclude.size} commits")
+    commits.get(branchName) match {
+      case Some(commits) => commits.filterNot(commitsToExclude.contains)
+      case None => List.empty
+    }
+  }
+
+  def getBranchPointer(branchName: String): Option[String] = commits.get(branchName).map(_.head)
+
+  def initializeWith(repository: Repository, lastKnownBranchPointers: Map[String, String]) {
+    logger.debug(s"Initializing repo cache")
+    val loadResult = repository.loadLastKnownRepoState(lastKnownBranchPointers, MaxCommitsPerBranchCount)
+    loadResult.commits.foreach { branchCommits =>
+      val commitsShas = branchCommits.commits.map(_.sha)
+      logger.debug(s"Adding ${commitsShas.size} commits to ${branchCommits.branchName}")
+      addCommitsToBranch(commitsShas, branchCommits.branchName)
+    }
+    logger.debug(s"Cache initialized")
+  }
+
+}
