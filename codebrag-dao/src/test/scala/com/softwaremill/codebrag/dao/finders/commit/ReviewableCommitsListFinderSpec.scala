ee4d524203afbacf84670119316ca4676b8f0304
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-06 21:14:07 +0100

    Fix tests after CommitInfo id generation on save

--- codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/finders/commit/ReviewableCommitsListFinderSpec.scala
+++ codebrag-dao/src/test/scala/com/softwaremill/codebrag/dao/finders/commit/ReviewableCommitsListFinderSpec.scala
@@ -3,38 +3,35 @@ package com.softwaremill.codebrag.dao.finders.commit
 import com.softwaremill.codebrag.dao._
 import org.scalatest.matchers.ShouldMatchers
 import com.softwaremill.codebrag.domain.builder.{UserAssembler, CommitInfoAssembler}
-import com.softwaremill.codebrag.common.LoadMoreCriteria
+import com.softwaremill.codebrag.common.{ClockSpec, LoadMoreCriteria}
 import com.softwaremill.codebrag.domain.{User, CommitInfo}
 import org.bson.types.ObjectId
 import com.softwaremill.codebrag.common.LoadMoreCriteria.PagingDirection
-import com.softwaremill.codebrag.dao.user.MongoUserDAO
-import com.softwaremill.codebrag.test.{FlatSpecWithMongo, ClearMongoDataAfterTest}
-import com.softwaremill.codebrag.dao.commitinfo.MongoCommitInfoDAO
-import com.softwaremill.codebrag.dao.reviewtask.{CommitReviewTaskDAO, MongoCommitReviewTaskDAO}
+import com.softwaremill.codebrag.dao.user.{SQLUserDAO, UserDAO, MongoUserDAO}
+import com.softwaremill.codebrag.test.{FlatSpecWithSQL, ClearSQLDataAfterTest, FlatSpecWithMongo, ClearMongoDataAfterTest}
+import com.softwaremill.codebrag.dao.commitinfo.{SQLCommitInfoDAO, CommitInfoDAO, MongoCommitInfoDAO}
+import com.softwaremill.codebrag.dao.reviewtask.{SQLCommitReviewTaskDAO, CommitReviewTaskDAO, MongoCommitReviewTaskDAO}
 import org.mockito.Mockito._
 import com.softwaremill.codebrag.domain.CommitReviewTask
 import org.scalatest.mock.MockitoSugar
+import org.scalatest.FlatSpec
 
-class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoDataAfterTest with ShouldMatchers with MockitoSugar {
+trait ReviewableCommitsListFinderSpec extends FlatSpec with ShouldMatchers with MockitoSugar {
 
   var finder: ReviewableCommitsListFinder = _
 
-  val commitDao = new MongoCommitInfoDAO
-  val reviewTaskDao = new MongoCommitReviewTaskDAO
-  val userDao = new MongoUserDAO
+  def commitDao: CommitInfoDAO
+  def reviewTaskDao: CommitReviewTaskDAO
+  def userDao: UserDAO
 
   val reviewingUserId = ObjectIdTestUtils.oid(100)
   val paging = LoadMoreCriteria.fromBeginning(2)
 
   val commitAuthor = UserAssembler.randomUser.withAvatarUrl("http://avatar.com").withFullName("John Doe").get
 
-  val commitOne = CommitInfoAssembler.randomCommit.withAuthorName(commitAuthor.name).get
-  val commitTwo = CommitInfoAssembler.randomCommit.withAuthorName(commitAuthor.name).get
-  val commitThree = CommitInfoAssembler.randomCommit.withAuthorName(commitAuthor.name).get
-
   it should "find no commits when user has no commits to review" taggedAs RequiresDb in {
     // given
-    storeCommits(commitOne, commitTwo, commitThree)
+    storeCommits(3, commitAuthor)
 
     initFinder(Set())
 
@@ -47,7 +44,7 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
 
   it should "find commits when user has commits to review" taggedAs RequiresDb in {
     // given
-    storeCommits(commitOne, commitTwo, commitThree)
+    val List(commitOne, _, commitThree) = storeCommits(3, commitAuthor)
     storeReviewTasksFor(reviewingUserId, commitOne, commitThree)
 
     initFinder(Set(commitOne.id, commitThree.id))
@@ -61,7 +58,7 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
 
   it should "indicate that there are no previous commits" taggedAs RequiresDb in {
     // given
-    storeCommits(commitOne, commitTwo, commitThree)
+    val List(commitOne, commitTwo, commitThree) = storeCommits(3, commitAuthor)
     storeReviewTasksFor(reviewingUserId, commitOne, commitTwo, commitThree)
 
     initFinder(Set(commitOne.id, commitTwo.id, commitThree.id))
@@ -76,7 +73,7 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
 
   it should "return information about how many more commits there are to review" taggedAs RequiresDb in {
     // given
-    storeCommits(commitOne, commitTwo, commitThree)
+    val List(commitOne, commitTwo, commitThree) = storeCommits(3, commitAuthor)
     storeReviewTasksFor(reviewingUserId, commitOne, commitTwo, commitThree)
 
     initFinder(Set(commitOne.id, commitTwo.id, commitThree.id))
@@ -91,7 +88,7 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
 
   it should "return page of next commits to review" taggedAs RequiresDb in {
     // given
-    storeCommits(commitOne, commitTwo, commitThree)
+    val List(commitOne, commitTwo, commitThree) = storeCommits(3, commitAuthor)
     storeReviewTasksFor(reviewingUserId, commitOne, commitTwo, commitThree)
 
     initFinder(Set(commitOne.id, commitTwo.id, commitThree.id))
@@ -107,7 +104,7 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
   it should "return commits view containing user data" taggedAs RequiresDb in {
     // given
     storeUser(commitAuthor)
-    storeCommits(commitOne)
+    val List(commitOne) = storeCommits(1, commitAuthor)
     storeReviewTasksFor(reviewingUserId, commitOne)
 
     initFinder(Set(commitOne.id))
@@ -130,9 +127,28 @@ class ReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoD
 
   private def storeUser(user: User) = userDao.add(user)
 
-  private def storeCommits(commits: CommitInfo*) = commits.foreach(commitDao.storeCommit)
+  private def storeCommits(count: Int, commitAuthor: User) = {
+    val storedCommits = for(i <- 1 to count) yield {
+      val commit = CommitInfoAssembler.randomCommit.withAuthorName(commitAuthor.name).get
+      commitDao.storeCommit(commit)
+    }
+    storedCommits.toList
+  }
 
   private def storeReviewTasksFor(userId: ObjectId, commits: CommitInfo*) = {
     commits.foreach( commit => reviewTaskDao.save(CommitReviewTask(commit.id, userId)))
   }
+}
+
+
+class MongoReviewableCommitsListFinderSpec extends FlatSpecWithMongo with ClearMongoDataAfterTest with ReviewableCommitsListFinderSpec {
+  val commitDao = new MongoCommitInfoDAO
+  val reviewTaskDao = new MongoCommitReviewTaskDAO
+  val userDao = new MongoUserDAO
+}
+
+class SQLReviewableCommitsListFinderSpec extends FlatSpecWithSQL with ClearSQLDataAfterTest with ReviewableCommitsListFinderSpec with ClockSpec {
+  val commitDao = new SQLCommitInfoDAO(sqlDatabase)
+  val reviewTaskDao = new SQLCommitReviewTaskDAO(sqlDatabase, clock)
+  val userDao = new SQLUserDAO(sqlDatabase)
 }
