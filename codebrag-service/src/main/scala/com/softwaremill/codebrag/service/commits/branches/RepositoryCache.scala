9581be6428475527af73c871924ec997c83c08bf
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-11 13:02:51 +0100

    Extend commits cache to store also author name and email in order to match user's commits

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/branches/RepositoryCache.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/branches/RepositoryCache.scala
@@ -2,8 +2,18 @@ package com.softwaremill.codebrag.service.commits.branches
 
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.repository.Repository
-import com.softwaremill.codebrag.domain.MultibranchLoadCommitsResult
+import com.softwaremill.codebrag.domain.{CommitLike, PartialCommitInfo, MultibranchLoadCommitsResult}
 
+case class CommitCacheEntry(sha: String, authorName: String, authorEmail: String)
+
+object CommitCacheEntry {
+
+  implicit object CommitLikeCommitCacheEntry extends CommitLike[CommitCacheEntry] {
+    def authorName(commitLike: CommitCacheEntry) = commitLike.authorName
+    def authorEmail(commitLike: CommitCacheEntry) = commitLike.authorEmail
+  }
+
+}
 
 /**
  * Keeps commits (SHA) for all repository branches
@@ -15,17 +25,17 @@ class RepositoryCache(backend: PersistentBackendForCache) extends Logging {
 
 
   // TODO: probably change this
-  private val commits = new scala.collection.mutable.HashMap[String, List[String]]
+  private val commits = new scala.collection.mutable.HashMap[String, List[CommitCacheEntry]]
 
   def addCommits(loadResult: MultibranchLoadCommitsResult) {
     backend.persist(loadResult)
     loadResult.commits.foreach { branchCommits =>
-      val commitsShas = branchCommits.commits.map(_.sha)
-      addCommitsToBranch(commitsShas, branchCommits.branchName)
+      val cacheEntries = branchCommits.commits.map(partialCommitToCacheEntry)
+      addCommitsToBranch(cacheEntries, branchCommits.branchName)
     }
   }
 
-  private def addCommitsToBranch(newCommits: List[String], branchName: String) {
+  private def addCommitsToBranch(newCommits: List[CommitCacheEntry], branchName: String) {
     logger.debug(s"Adding ${newCommits.size} to ${branchName}")
     val finalCommits = commits.get(branchName) match {
       case Some(commits) => newCommits ::: commits
@@ -36,12 +46,9 @@ class RepositoryCache(backend: PersistentBackendForCache) extends Logging {
     logger.debug(s"Final number of commits in ${branchName}: ${commits.get(branchName).getOrElse(List.empty).size}")
   }
 
-  def getCommitsForBranchExcluding(commitsToExclude: List[String], branchName: String): List[String] = {
-    logger.debug(s"Getting branch commits for ${branchName} and excluding ${commitsToExclude.size} commits")
-    commits.get(branchName) match {
-      case Some(commits) => commits.filterNot(commitsToExclude.contains)
-      case None => List.empty
-    }
+  def getBranchCommits(branchName: String): List[CommitCacheEntry] = {
+    logger.debug(s"Getting branch commits for ${branchName}")
+    commits.get(branchName).getOrElse(List.empty[CommitCacheEntry])
   }
 
   def initializeWith(repository: Repository) {
@@ -49,11 +56,15 @@ class RepositoryCache(backend: PersistentBackendForCache) extends Logging {
     val savedState = backend.loadBranchesState()
     val loadResult = repository.loadLastKnownRepoState(savedState, MaxCommitsPerBranchCount)
     loadResult.commits.foreach { branchCommits =>
-      val commitsShas = branchCommits.commits.map(_.sha)
-      logger.debug(s"Adding ${commitsShas.size} commits to ${branchCommits.branchName}")
-      addCommitsToBranch(commitsShas, branchCommits.branchName)
+      val cacheEntries = branchCommits.commits.map(partialCommitToCacheEntry)
+      logger.debug(s"Adding ${cacheEntries.size} commits to ${branchCommits.branchName}")
+      addCommitsToBranch(cacheEntries, branchCommits.branchName)
     }
     logger.debug(s"Cache initialized")
   }
+
+  private def partialCommitToCacheEntry(commit: PartialCommitInfo): CommitCacheEntry = {
+    CommitCacheEntry(commit.sha, commit.authorName, commit.authorEmail)
+  } 
 
 }
