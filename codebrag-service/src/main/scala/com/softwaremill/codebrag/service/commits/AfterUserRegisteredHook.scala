dc0cae96e2af5d3f6be33cfbb766f3b43d4c3063
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-17 14:57:32 +0100

    When user registers mark his/her starting date. Before that date commits will not be shown to review.
    This is either date of earliest commit from 10 last commits made by other users or date being 7 days back since user registered (if there are no user commits in current branch)

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/AfterUserRegisteredHook.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/commits/AfterUserRegisteredHook.scala
@@ -3,37 +3,49 @@ package com.softwaremill.codebrag.service.commits
 import com.softwaremill.codebrag.domain._
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.dao.events.NewUserRegistered
-import com.softwaremill.codebrag.service.commits.branches.{ReviewedCommitsCache, RepositoryCache}
+import com.softwaremill.codebrag.service.commits.branches.{CommitCacheEntry, ReviewedCommitsCache, RepositoryCache}
 import CommitAuthorClassification._
+import org.joda.time.{DateTimeUtils, DateTime}
 
-class AfterUserRegisteredHook(val repoCache: RepositoryCache, val reviewedCommitsCache: ReviewedCommitsCache) extends PrepareCommitsToReview {
+class AfterUserRegisteredHook(
+  val repoCache: RepositoryCache,
+  val reviewedCommitsCache: ReviewedCommitsCache) extends SetStartingDateForUser {
 
   def run(user: NewUserRegistered) {
-    prepareCommitsToReviewFor(user)
+    setStartingDate(user)
   }
 
 }
 
-trait PrepareCommitsToReview extends Logging {
+trait SetStartingDateForUser extends Logging {
 
   val repoCache: RepositoryCache
   val reviewedCommitsCache: ReviewedCommitsCache
 
   private val LastCommitsToReviewCount = 10
+  private val DaysBackToTakeCommits = 7
 
-  protected def prepareCommitsToReviewFor(user: NewUserRegistered) {
-    logger.debug(s"New user ${user.login} registered. Preparing reviewed/to review commits")
-    val commitsToLeaveForReview = repoCache.getBranchNames.map { branch =>
-      val branchCommits = repoCache.getBranchCommits(branch)
-        .filterNot(commitAuthoredByUser(_, user))
-        .take(LastCommitsToReviewCount)
-      logger.debug(s"Commits left to review for branch ${branch} (${branchCommits.size}): ${branchCommits.map(_.sha.substring(0, 5))}")
-      branchCommits
-    }.flatten.toSet
-    logger.debug(s"${commitsToLeaveForReview.size} commit(s) left for review for new user ${user.login}")
-    val toMarkAsReviewed = repoCache.getAllCommits.filterNot(commitsToLeaveForReview.contains)
-    logger.debug(s"${toMarkAsReviewed.size} commit(s) to mark as reviewed for new user ${user.login}")
-    reviewedCommitsCache.markCommitsAsReviewedBy(user.userId, toMarkAsReviewed)
+  protected def setStartingDate(user: NewUserRegistered) {
+    val weekBeforeRegistration = user.timestamp.minusDays(DaysBackToTakeCommits)
+    val allBranchCommits = repoCache.getBranchCommits(currentBranch)
+    val newestForUserToReview = allBranchCommits.filterNot(commitAuthoredByUser(_, user)).take(LastCommitsToReviewCount)
+    val dateBoundary = newestForUserToReview.lastOption match {
+      case Some(last) => getLatestFromBothDates(last.commitDate, weekBeforeRegistration)
+      case None => weekBeforeRegistration
+    }
+    reviewedCommitsCache.setStartingDateForUser(dateBoundary, user.id)
+    logger.debug(s"New user ${user.login} registered. To review boundary date set to ${dateBoundary}")
   }
+
+
+  def getLatestFromBothDates(lastCommitDate: DateTime, weekBeforeRegistration: DateTime): DateTime = {
+    if (lastCommitDate.isBefore(weekBeforeRegistration)) {
+      weekBeforeRegistration
+    } else {
+      lastCommitDate
+    }
+  }
+
+  private def currentBranch = repoCache.repository.getCheckedOutBranchName
 
 }
