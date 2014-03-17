d6bb8aa7c0d2f6470208407fe87605f24532aef1
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-03-17 14:34:02 +0100

    Determine currently checked out branch

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/repository/Repository.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/repository/Repository.scala
@@ -3,12 +3,12 @@ package com.softwaremill.codebrag.repository
 import com.typesafe.scalalogging.slf4j.Logging
 import org.eclipse.jgit.lib.{ObjectId, Constants}
 import org.eclipse.jgit.revwalk.{RevWalk, RevCommit}
-import org.eclipse.jgit.storage.file.{FileRepository, FileRepositoryBuilder}
-import java.io.File
+import org.eclipse.jgit.storage.file.FileRepository
 import org.eclipse.jgit.errors.MissingObjectException
 import com.softwaremill.codebrag.repository.config.RepoData
+import org.eclipse.jgit.api.Git
 
-trait Repository extends Logging with RepositorySnapshotLoader with RepositoryDeltaLoader {
+trait Repository extends Logging with RepositorySnapshotLoader with RepositoryDeltaLoader with BranchListModeSelector {
 
   def repoData: RepoData
   def repo: FileRepository
@@ -38,6 +38,16 @@ trait Repository extends Logging with RepositorySnapshotLoader with RepositoryDe
       } catch {
         case e: MissingObjectException => None
       }
+    }
+  }
+
+  def getCheckedOutBranchName = {
+    val shortBranchName = repo.getBranch
+    import scala.collection.JavaConversions._
+    val branches = (new Git(repo)).branchList().setListMode(branchListMode).call().toList
+    branches.map(_.getName).find(_.endsWith("/" + shortBranchName)) match {
+      case Some(branch) => branch
+      case None => throw new RuntimeException("Cannot determine currently checked out branch")
     }
   }
 
