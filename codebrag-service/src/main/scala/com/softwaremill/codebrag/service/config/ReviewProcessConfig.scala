55823cc1b58c8163df2695b88ea3417dff15829c
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-04-17 14:28:58 +0200

    required-reviewers-count now supports both integers and "all"

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/ReviewProcessConfig.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/config/ReviewProcessConfig.scala
@@ -1,14 +1,29 @@
 package com.softwaremill.codebrag.service.config
 
 import com.softwaremill.codebrag.common.config.ConfigWithDefault
+import com.typesafe.config.Config
 
 trait ReviewProcessConfig extends ConfigWithDefault {
-
-  lazy val requiredReviewersCount = getInt("codebrag.required-reviewers-count", 1)
-
+  lazy val requiredReviewersCount = ReviewersCount.getFrom(rootConfig, "codebrag.required-reviewers-count")
   lazy val initialCommitsToReviewCount = getInt("codebrag.initial-to-review", 10)
   lazy val initialToReviewDays = getInt("codebrag.initial-days-to-review", 30)
 
   lazy val daysToRecreateFollowups = getInt("codebrag.days-to-recreate-followups", 30)
+}
 
+object ReviewersCount {
+  private val AllValue = Int.MaxValue
+  private val DefaultValue = 1
+
+  def getFrom(config: Config, key: String): Int = {
+    if(config.hasPath(key)) {
+      config.getValue(key).unwrapped() match {
+        case i: Integer => i
+        case s: String => AllValue
+        case _ => DefaultValue
+      }
+    } else {
+      DefaultValue
+    }
+  }
 }
