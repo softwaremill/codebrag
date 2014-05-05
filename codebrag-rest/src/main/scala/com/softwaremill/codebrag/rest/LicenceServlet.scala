09a0895100f81f6c75e371fd35445607f5ea8d66
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-05-05 12:45:09 +0200

    Register licence via API

--- codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/LicenceServlet.scala
+++ codebrag-rest/src/main/scala/com/softwaremill/codebrag/rest/LicenceServlet.scala
@@ -3,8 +3,12 @@ package com.softwaremill.codebrag.rest
 import com.typesafe.scalalogging.slf4j.Logging
 import com.softwaremill.codebrag.service.user.Authenticator
 import com.softwaremill.codebrag.licence.LicenceService
+import com.softwaremill.codebrag.service.comments.command.IncomingLike
+import org.bson.types.ObjectId
+import com.softwaremill.codebrag.activities.RegisterLicenceUseCase
+import org.scalatra
 
-class LicenceServlet(licenceService: LicenceService, val authenticator: Authenticator) extends JsonServletWithAuthentication with Logging {
+class LicenceServlet(licenceService: LicenceService, registerUseCase: RegisterLicenceUseCase, val authenticator: Authenticator) extends JsonServletWithAuthentication with Logging {
 
   get("/") {
     haltIfNotAuthenticated()
@@ -14,6 +18,16 @@ class LicenceServlet(licenceService: LicenceService, val authenticator: Authenti
       "daysLeft" -> licenceService.daysToExpire,
       "type" -> licenceService.licenceType,
       "companyName" -> licenceService.companyName)
+  }
+
+  put("/") {
+    haltIfNotAuthenticated()
+    val licenceKey = (parsedBody \ "licenceKey").extract[String]
+    logger.debug(s"Trying to register licence key ${licenceKey}")
+    registerUseCase.execute(licenceKey) match {
+      case Right(licence) => scalatra.Ok(licence)
+      case Left(msg) => halt(400, Map("error" -> "Invalid licence key provided"))
+    }
   }
 
 }
