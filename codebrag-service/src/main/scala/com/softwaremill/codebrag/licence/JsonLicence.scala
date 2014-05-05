84daaa4b3c1d0e4acccada4ef931f7cc8c64a7b8
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-05-05 11:52:09 +0200

    Extract reading/writing licence to/from json

---
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/licence/JsonLicence.scala
@@ -0,0 +1,37 @@
+package com.softwaremill.codebrag.licence
+
+import org.joda.time.format.DateTimeFormat
+import org.json4s.ext.EnumNameSerializer
+import org.json4s.CustomSerializer
+import org.joda.time.DateTime
+import org.json4s.JsonAST.{JNull, JString}
+
+object LicenceAsJson {
+
+  private val Formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss:SSS")
+  implicit val JsonFormats = org.json4s.DefaultFormats + FullDateTimeSerializer + new EnumNameSerializer(LicenceType)
+
+  def toJson(licence: Licence) = {
+    import org.json4s.jackson.Serialization.write
+    write(licence)
+  }
+
+  def fromJson(string: String): Licence = {
+    import org.json4s._
+    import org.json4s.jackson.JsonMethods._
+    try {
+      parse(string).extract[Licence]
+    } catch {
+      case e: Exception => throw new InvalidLicenceKeyException(s"Invalid licence key provided ${string}")
+    }
+  }
+
+  private object FullDateTimeSerializer extends CustomSerializer[DateTime](format => ( {
+    case JString(d) => DateTime.parse(d, Formatter)
+    case JNull => null
+    }, {
+    case d: DateTime => JString(Formatter.print(d))
+    }
+  ))
+
+}
