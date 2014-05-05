7c88ec194dc06461b244e149f47f61501138dfd6
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-05-05 20:22:10 +0200

    Cannot register licence with fewer users than already existing in app

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/activities/RegisterLicenceUseCase.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/activities/RegisterLicenceUseCase.scala
@@ -2,16 +2,32 @@ package com.softwaremill.codebrag.activities
 
 import com.softwaremill.codebrag.licence._
 import com.typesafe.scalalogging.slf4j.Logging
+import com.softwaremill.codebrag.dao.user.UserDAO
 
-class RegisterLicenceUseCase(licenceService: LicenceService) extends Logging {
+class RegisterLicenceUseCase(licenceService: LicenceService, userDao: UserDAO) extends Logging {
 
   type EnterLicenceKeyResult = Either[String, Licence]
   
   def execute(providedKey: String): EnterLicenceKeyResult = {
+    validate(providedKey).right.flatMap { licence =>
+      licenceService.updateLicence(licence)
+      Right(licence)
+    }
+  }
+
+  def validate(providedKey: String): EnterLicenceKeyResult = {
+    decodeKey(providedKey).right.flatMap { licence =>
+      if(userDao.countAll() > licence.maxUsers) {
+        Left("Invalid licence - too many users registered")
+      } else {
+        Right(licence)
+      }
+    }
+  }
+
+  private def decodeKey(providedKey: String): EnterLicenceKeyResult = {
     try {
-      val licenceDetails = LicenceEncryptor.decode(providedKey)
-      licenceService.updateLicence(licenceDetails)
-      Right(licenceDetails)
+      Right(LicenceEncryptor.decode(providedKey))
     } catch {
       case e: InvalidLicenceKeyException => {
         logger.error("Could not register provided key", e.getMessage)
--- codebrag-service/src/test/scala/com/softwaremill/codebrag/activities/RegisterLicenceUseCaseSpec.scala
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/activities/RegisterLicenceUseCaseSpec.scala
@@ -4,17 +4,22 @@ import org.scalatest.{BeforeAndAfter, FlatSpec}
 import com.softwaremill.codebrag.licence.{LicenceEncryptor, Licence, LicenceService}
 import org.scalatest.mock.MockitoSugar
 import com.softwaremill.codebrag.common.ClockSpec
-import org.mockito.Mockito
+import org.mockito.Mockito._
 import org.scalatest.matchers.ShouldMatchers
+import com.softwaremill.codebrag.dao.user.UserDAO
 
 class RegisterLicenceUseCaseSpec extends FlatSpec with BeforeAndAfter with MockitoSugar with ClockSpec with ShouldMatchers {
 
   var licenceService: LicenceService = _
+  var userDao: UserDAO = _
   var useCase: RegisterLicenceUseCase = _
 
   before {
     licenceService = mock[LicenceService]
-    useCase = new RegisterLicenceUseCase(licenceService)
+    userDao = mock[UserDAO]
+    useCase = new RegisterLicenceUseCase(licenceService, userDao)
+
+    when(userDao.countAll()).thenReturn(1)
   }
 
   it should "update existing licence in Codebrag" in {
@@ -26,8 +31,22 @@ class RegisterLicenceUseCaseSpec extends FlatSpec with BeforeAndAfter with Mocki
     val Right(result) = useCase.execute(encodedLicence)
 
     // then
-    Mockito.verify(licenceService).updateLicence(newLicence)
+    verify(licenceService).updateLicence(newLicence)
     result should be(newLicence)
+  }
+
+  it should "not update existing licence when current licence has fewer users allowed than current users count" in {
+    // given
+    val newLicence = Licence(clock.now.plusDays(30), 10, "SoftwareMill")
+    when(userDao.countAll()).thenReturn(newLicence.maxUsers + 1)
+    val encodedLicence = LicenceEncryptor.encode(newLicence)
+
+    // when
+    val result = useCase.execute(encodedLicence)
+
+    // then
+    verifyZeroInteractions(licenceService)
+    result should be('left)
   }
 
   it should "not update existing licence when invalid JSON key provided" in {
@@ -38,7 +57,7 @@ class RegisterLicenceUseCaseSpec extends FlatSpec with BeforeAndAfter with Mocki
     val result = useCase.execute(invalidLicenceString)
 
     // then
-    Mockito.verifyZeroInteractions(licenceService)
+    verifyZeroInteractions(licenceService)
     result should be('left)
   }
 
