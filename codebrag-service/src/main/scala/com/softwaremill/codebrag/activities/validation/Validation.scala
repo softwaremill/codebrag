b87131ca9cf84fe05f67e27bb368e537138c0dc5
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-06-04 08:31:00 +0200

    Simplified Validation constructors

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/activities/validation/Validation.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/activities/validation/Validation.scala
@@ -13,7 +13,6 @@ case class ValidationErrors(fieldErrors: Map[String, List[String]]) {
 }
 
 case class Validation(checks: List[ValidationRule]) {
-  def this(checks: ValidationRule*) = this(checks.toList)
   def + (additionalChecks: ValidationRule*) = Validation(checks ++ additionalChecks)
   lazy val errors = ValidationErrors(checks.toList.filter(_._1).groupBy(_._3).mapValues(_.map(_._2)))
   def whenNoErrors[T](block: => T): Either[ValidationErrors, T] = if(errors.isEmpty) Right(block) else Left(errors)
@@ -21,4 +20,5 @@ case class Validation(checks: List[ValidationRule]) {
 
 object Validation {
   type ValidationRule = (Boolean, String, String)
+  def apply(checks: ValidationRule*) = new Validation(checks.toList)
 }
