f86149bdc412a6dca4458f1771861b2727f01ec6
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-06-17 23:44:02 +0200

    Branches controller uses new repo context to display/manage branches and switching filters

--- codebrag-ui/test/branches/branchesCtrl-spec.js
+++ codebrag-ui/test/branches/branchesCtrl-spec.js
@@ -1,62 +1,83 @@
 describe("Branches Controller", function () {
 
-    var $q, $scope, $controller, $rootScope;
-    var branchesService, countersService, events;
+    var $q, $scope, $controller,
+        branchesService, events,
+        allBranches = ['master', 'feature', 'bugfix'];
 
-    beforeEach(module('codebrag.branches'));
+    beforeEach(module('codebrag.branches', 'codebrag.counters'));
 
-    beforeEach(inject(function (_$q_, _branchesService_, _countersService_, _$rootScope_, _$controller_, _events_) {
-        $q = _$q_;
-        $rootScope = _$rootScope_;
-        $scope = _$rootScope_.$new();
-        branchesService = _branchesService_;
-        countersService = _countersService_;
-        $controller = _$controller_;
-        events = _events_;
+    beforeEach(inject(function($injector) {
+        $q = $injector.get('$q');
+        $scope = $injector.get('$rootScope').$new();
+        $controller = $injector.get('$controller');
+        events = $injector.get('events');
     }));
+
+    beforeEach(function() {
+        branchesService = {
+            loadBranches: function() { return $q.when(allBranches) },
+            repoType: function() { return 'git' }
+        };
+    });
 
     it('should load all branches on start', function() {
         // given
-        var allBranches = ['master', 'feature', 'bugfix'];
-        var allBranchesPromise = $q.when(allBranches);
-        spyOn(branchesService, 'fetchBranches').andReturn(allBranchesPromise);
+        var countersService = {},
+            currentRepoContext = {};
 
         // when
-        $controller('BranchesCtrl', {$scope: $scope, branchesService: branchesService});
-        $scope.$apply();
+        createController($scope, branchesService, countersService, currentRepoContext);
+        $scope.$digest();
 
         // then
         expect($scope.branches).toBe(allBranches);
     });
 
-    it('should change current branch', function() {
+    it('should change current branch and commits list filter', function() {
         // given
-        var allBranches = ['master', 'feature', 'bugfix'];
-        var allBranchesPromise = $q.when(allBranches);
-        spyOn(branchesService, 'fetchBranches').andReturn(allBranchesPromise);
-        spyOn(branchesService, 'selectBranch');
+        var countersService = {},
+            currentRepoContext = jasmine.createSpyObj('currentRepo', ['switchBranch', 'switchCommitsFilter']);
+
+        createController($scope, branchesService, countersService, currentRepoContext);
 
         // when
-        $controller('BranchesCtrl', {$scope: $scope, branchesService: branchesService});
-        $scope.$apply();
         $scope.selectBranch("feature");
+        $scope.switchListView("all");
 
         // then
-        expect(branchesService.selectBranch).toHaveBeenCalledWith("feature");
+        expect(currentRepoContext.switchBranch).toHaveBeenCalledWith("feature");
+        expect(currentRepoContext.switchCommitsFilter).toHaveBeenCalledWith("all");
     });
 
-    it('should return correct label (with counter) for branch and commits to review', function() {
+    it('should return correct label (with counter) for branch and commits to review', inject(function(Counter) {
         // given
-        spyOn(countersService.commitsCounter, 'currentCount').andReturn(20);
-        $controller('BranchesCtrl', {$scope: $scope});
+        var currentRepoContext, countersService;
+
+        currentRepoContext = {
+            commitsFilter: 'pending',
+            isToReviewFilterSet: function() {
+                return true
+            }
+        };
+        countersService = {
+            commitsCounter: new Counter(10)
+        };
+
+        createController($scope, branchesService, countersService, currentRepoContext);
 
         // when
         var toReviewLabel = $scope.displaySelectedMode();
-        $scope.switchListView('all');
-        var allLabel = $scope.displaySelectedMode();
 
         // then
-        expect(toReviewLabel).toBe('to review (20)');
-        expect(allLabel).toBe('all');
-    });
+        expect(toReviewLabel).toBe('to review (10)');
+    }));
+
+    function createController(scope, branchesService, countersService, currentRepoContext) {
+        $controller('BranchesCtrl', {
+            $scope: scope,
+            branchesService: branchesService,
+            countersService: countersService,
+            currentRepoContext: currentRepoContext
+        });
+    }
 });
