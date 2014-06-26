569c898354462972fb793ab4a5a70d8d0f144fc0
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-06-26 14:42:31 +0200

    Introduce repo selector

---
+++ codebrag-ui/app/scripts/branches/reposCtrl.js
@@ -0,0 +1,22 @@
+angular.module('codebrag.branches')
+
+    .controller('ReposCtrl', function ($scope, $state, events, currentRepoContext) {
+
+        $scope.currentRepoContext = currentRepoContext;
+
+        $scope.selectRepo = function(selected) {
+            currentRepoContext.switchRepo(selected);
+            $state.transitionTo('commits.list', {repo: selected});
+        };
+
+        $scope.isSelected = function(repo) {
+            return currentRepoContext.repo === repo;
+        };
+
+        $scope.selectedBranch = function() {
+            return currentRepoContext.repo;
+        };
+
+    });
+
+
