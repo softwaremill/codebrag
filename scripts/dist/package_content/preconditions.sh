9df74416454ebee354a8355811b07f064a200327
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-07-01 12:41:07 +0200

    Remove "only 1 repo" check from startup script

--- scripts/dist/package_content/preconditions.sh
+++ scripts/dist/package_content/preconditions.sh
@@ -13,10 +13,9 @@ function java_not_installed {
 
 # 0. check if REPOS_DIR defined in config and set it accordingly 
 # 1. check if root dir with repos exists
-# 2. check if it contains exactly 1 repo dir
-# 3. check if repo dir is readable by current user
-# 4. check if it is Git repo (check for .git dir)
-# 5. check if repo dir is writable by current user
+# 2. check if repo dir is readable by current user
+# 3. check if it is Git repo (check for .git dir)
+# 4. check if repo dir is writable by current user
 function check_repo_available {
   local CWD=$(pwd)
 
@@ -36,13 +35,6 @@ function check_repo_available {
  git: git clone git://yourcompany.com/path/to/project-abc.git
  svn: git svn clone http://yourcompany.com/path/to/svn/project-abc project-abc    # don't use 'svn checkout'"
     exit 1    
-  fi
-
-  # check if exactly 1 repository directory is present
-  if [ $SUBDIRS_COUNT -ne 1 ]; then
-    echo "ERROR: $REPOS_DIR directory should contain exactly one directory with your repository ($SUBDIRS_COUNT directories found).
- At the moment Codebrag supports single repository configuration only."
-    exit 1
   fi
 
   local POTENTIAL_REPO_DIR=$(find $REPOS_DIR -maxdepth 1 -mindepth 1 -type d -print)
