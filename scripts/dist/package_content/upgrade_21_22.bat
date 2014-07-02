d4c2c09487b63c0a924884c40c5bc18294dd8c9b
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-07-02 11:44:00 +0200

    Rename migration scripts

--- scripts/dist/package_content/upgrade_21_22.bat
+++ scripts/dist/package_content/upgrade_21_22.bat
@@ -1,2 +1,2 @@
-echo "Upgrading Codebrag 2.0 to 2.1"
+echo "Upgrading Codebrag 2.1 to 2.2"
 java -Dconfig.file=codebrag.conf -cp codebrag.jar com.softwaremill.codebrag.migration.MigrateV2_1ToV2_2
--- scripts/dist/package_content/upgrade_21_22.sh
+++ scripts/dist/package_content/upgrade_21_22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Upgrading Codebrag 2.0 to 2.1"
+echo "Upgrading Codebrag 2.1 to 2.2"
 CONFIG=${1:-"codebrag.conf"}
 
 java -Dconfig.file=$CONFIG -cp codebrag.jar com.softwaremill.codebrag.migration.MigrateV2_1ToV2_2
