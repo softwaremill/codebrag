a42f2ca1bc5e7bfda8a2434898daead4dfc404ae
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-07-10 14:08:49 +0200

    Check on multiple emails if commit authored by user

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/CommitAuthorClassification.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/CommitAuthorClassification.scala
@@ -2,7 +2,6 @@ package com.softwaremill.codebrag.domain
 
 object CommitAuthorClassification {
   def commitAuthoredByUser[T, S](commit: T, user: S)(implicit commitLike: CommitLike[T], userLike: UserLike[S]): Boolean = {
-    commitLike.authorName(commit) == userLike.userFullName(user) ||
-      commitLike.authorEmail(commit) == userLike.userEmails(user)
+    commitLike.authorName(commit) == userLike.userFullName(user) || userLike.userEmails(user).contains(commitLike.authorEmail(commit))
   }
 }
