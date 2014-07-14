f637ea7c8e86db59b6a8a9f43b1b2104857d5145
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-07-14 15:09:24 +0200

    Add user aliases table

---
+++ codebrag-dao/src/main/resources/db/migration/V16__add_user_aliases_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS "user_aliases"(
+    "id" VARCHAR NOT NULL,
+    "user_id" VARCHAR NOT NULL,
+    "alias" VARCHAR NOT NULL
+);
+ALTER TABLE "user_aliases" ADD CONSTRAINT IF NOT EXISTS "user_alias_pk" PRIMARY KEY("id");
+ALTER TABLE "user_aliases" ADD CONSTRAINT IF NOT EXISTS "user_alias_pk" UNIQUE("user_id", "alias");
