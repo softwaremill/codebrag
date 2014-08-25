7fc12488eb29d289334a8b2efb54a62c5fbd6d1c
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-08-25 13:16:20 +0200

    Fix - get rid of case sensitive matching of commit author. Now it is case insensitive

--- codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/CommitAuthorClassification.scala
+++ codebrag-domain/src/main/scala/com/softwaremill/codebrag/domain/CommitAuthorClassification.scala
@@ -2,6 +2,8 @@ package com.softwaremill.codebrag.domain
 
 object CommitAuthorClassification {
   def commitAuthoredByUser[T, S](commit: T, user: S)(implicit commitLike: CommitLike[T], userLike: UserLike[S]): Boolean = {
-    commitLike.authorName(commit) == userLike.userFullName(user) || userLike.userEmails(user).contains(commitLike.authorEmail(commit))
+    val nameMatches = commitLike.authorName(commit).toLowerCase == userLike.userFullName(user).toLowerCase
+    val emailMatches = userLike.userEmails(user).map(_.toLowerCase).contains(commitLike.authorEmail(commit).toLowerCase)
+    nameMatches || emailMatches
   }
 }
