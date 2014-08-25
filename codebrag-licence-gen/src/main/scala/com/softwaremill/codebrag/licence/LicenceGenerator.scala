fe281744e44e55601a895c58b58b5703ca0a2685
mchmielarz <michal.chmielarz@gmail.com>
2014-08-25 10:48:17 +0200

    Generation of Codebrag's licence extracted to separate class.
    Current LicenceGenerator renamed to LicenceGeneratorApp.

--- codebrag-licence-gen/src/main/scala/com/softwaremill/codebrag/licence/LicenceGenerator.scala
+++ codebrag-licence-gen/src/main/scala/com/softwaremill/codebrag/licence/LicenceGenerator.scala
@@ -1,67 +1,10 @@
 package com.softwaremill.codebrag.licence
 
-import org.joda.time.format.DateTimeFormat
 import org.joda.time.DateTime
 
-/**
- * Super awesome licence generation tool
- */
-object LicenceGenerator extends App {
+class LicenceGenerator {
 
-  private val Formatter = DateTimeFormat.forPattern("dd/MM/yyyy")
-
-  println("--------------------------------")
-  println("Codebrag licence generation tool")
-  println("--------------------------------")
-
-  val date = enterExpirationDate
-  val users = enterUsersCount
-  val company = enterCompanyName
-
-  val licence = generateLicence(date, users, company)
-
-  println
-  println("Generated licence key:")
-  println(licence)
-  println("Thanks, exiting.")
-
-  private def enterExpirationDate: DateTime = {
-    print("Enter expiration date (dd/MM/yyyy)> ")
-    val expDateString = readLine()
-    try {
-      DateTime.parse(expDateString, Formatter)
-    } catch {
-      case e: Exception => {
-        println(s"Invalid date: ${expDateString}")
-        enterExpirationDate
-      }
-    }
-  }
-
-  private def enterUsersCount: Int = {
-    print("Enter users count> ")
-    val usersCountString = readLine()
-    try {
-      usersCountString.toInt
-    } catch {
-      case e: Exception => {
-        println(s"Invalid users count: ${usersCountString}")
-        enterUsersCount
-      }
-    }
-  }
-
-  private def enterCompanyName: String = {
-    print("Enter company name > ")
-    val companyName = readLine()
-    if(companyName.trim.isEmpty) {
-      println("Company name is required")
-      enterCompanyName
-    }
-    companyName
-  }
-
-  private def generateLicence(date: DateTime, users: Int, companyName: String): String = {
+  def generateLicence(date: DateTime, users: Int, companyName: String): String = {
     val licence = Licence(date.withTime(23, 59, 59, 999), users, companyName)
     LicenceEncryptor.encode(licence)
   }
