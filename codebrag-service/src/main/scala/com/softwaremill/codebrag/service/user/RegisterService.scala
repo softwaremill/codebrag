e9d4ab57c1a39265bca05390ee5faa1acbc29085
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-08-29 09:53:31 +0200

    A bit of DRY applied

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/service/user/RegisterService.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/service/user/RegisterService.scala
@@ -14,25 +14,21 @@ class RegisterService(userDao: UserDAO, newUserAdder: NewUserAdder, invitationSe
 
   def register(login: String, email: String, password: String, invitationCode: String): Either[String, Unit] = {
     logger.info(s"Trying to register $login")
-    val emailLowerCase = email.toLowerCase
+    val newUser = User(new ObjectId, Authentication.basic(login, password), login, email.toLowerCase, UUID.randomUUID().toString)
     if (firstRegistration) {
-      val firstUser = User(new ObjectId, Authentication.basic(login, password), login, emailLowerCase, UUID.randomUUID().toString)
-      registerUser(firstUser.makeAdmin)
+      registerUser(newUser.makeAdmin)
     } else {
-      registerUserWithInvitation(login, emailLowerCase, password, invitationCode)
+      registerUserWithInvitation(newUser, invitationCode)
     }
   }
 
-  private def registerUserWithInvitation(login: String, emailLowerCase: String, password: String, invitationCode: String): Either[String, Unit] = {
+  private def registerUserWithInvitation(newUser: User, invitationCode: String): Either[String, Unit] = {
     for {
       _ <- leftIfTrue(invitationCode.isEmpty, "To register in Codebrag you need a registration link. Ask another Codebrag user to send you one.").right
       _ <- leftIfTrue(!invitationService.verify(invitationCode), "The registration link is not valid or was already used. Ask your friend for another one.").right
-      _ <- leftIfSome(userDao.findByLowerCasedLogin(login), "User with the given login already exists").right
-      _ <- leftIfSome(userDao.findByEmail(emailLowerCase), "User with the given email already exists").right
-    } yield {
-      val user = User(new ObjectId, Authentication.basic(login, password), login, emailLowerCase, UUID.randomUUID().toString)
-      registerUser(user)
-    }
+      _ <- leftIfSome(userDao.findByLowerCasedLogin(newUser.name), "User with the given login already exists").right
+      _ <- leftIfSome(userDao.findByEmail(newUser.emailLowerCase), "User with the given email already exists").right
+    } yield registerUser(newUser)
   }
 
   private def registerUser(user: User): Either[String, Unit] = {
