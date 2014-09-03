e70c0d8afd7e2380340f00aa5bc9e445ed9a6812
Lukasz Lenart <lukasz.lenart@gmail.com>
2014-09-03 13:13:47 +0200

    Drops unused plugins and updates version of the rest

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -1,19 +1,13 @@
 import sbt._
-import Keys._
 
 object Plugins extends Build {
   lazy val plugins = Project(
     "plugins",
     file("."),
     settings = Defaults.defaultSettings ++ Seq(
-      addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.4.0"),
-      addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.0"),
-      addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.0.1"),
-      addSbtPlugin("com.github.philcali" % "sbt-jslint" % "0.1.3"),
-      addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0")))
-      .dependsOn(
-    // TeamCity reporting, see: https://github.com/guardian/sbt-teamcity-test-reporting-plugin
-    uri("git://github.com/guardian/sbt-teamcity-test-reporting-plugin.git#1.2")
+      addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4"),
+      addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.1.0"),
+      addSbtPlugin("com.gu" % "sbt-teamcity-test-reporting-plugin" % "1.5"),
+      addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0"))
   )
-
 }
