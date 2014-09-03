de66640fb199cdc0e3bcfcdfa8f9ac92ad0cb7cc
Lukasz Lenart <lukasz.lenart@gmail.com>
2014-09-03 13:14:50 +0200

    Replaces deprecated settings

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -4,7 +4,7 @@ object Plugins extends Build {
   lazy val plugins = Project(
     "plugins",
     file("."),
-    settings = Defaults.defaultSettings ++ Seq(
+    settings = Defaults.coreDefaultSettings ++ Seq(
       addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4"),
       addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.1.0"),
       addSbtPlugin("com.gu" % "sbt-teamcity-test-reporting-plugin" % "1.5"),
