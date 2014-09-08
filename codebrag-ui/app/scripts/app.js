8f5518d3f9a797baed3d139d765cad135705b321
Michal Ostruszka <michal.ostruszka@gmail.com>
2014-09-08 09:53:00 +0200

    Cleanup - remove old stuff, never needed.

--- codebrag-ui/app/scripts/app.js
+++ codebrag-ui/app/scripts/app.js
@@ -90,7 +90,7 @@ angular.module('codebrag.licence')
     });
 
 angular.module('codebrag.session')
-    .config(function ($stateProvider, $urlRouterProvider, authenticatedUser) {
+    .config(function ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.when('', '/');
         $stateProvider
             .state('home', {
@@ -108,16 +108,6 @@ angular.module('codebrag.session')
                 },
                 noLogin: true
             })
-            .state('profile', {
-                url: '/profile',
-                templateUrl: 'views/secured/profile.html',
-                resolve: authenticatedUser
-            })
-            .state('error', {
-                url: '/error',
-                templateUrl: 'views/errorpages/error500.html',
-                noLogin: true
-            });
     });
 
 angular.module('codebrag.commits')
