04e5f906bd4334e0d2c0fb3713e4e3aada48f822
Jacek Kunicki <jacek@kunicki.org>
2014-11-10 13:46:18 +0100

    Cosmetics (map before foreach, for comprehension instead of nested loops)

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/migration/FixZombieCommits.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/migration/FixZombieCommits.scala
@@ -16,38 +16,34 @@ object FixZombieCommits extends App with MigrationConfig with MultiRepoConfig {
   logger.debug("Fixing zombie commits - Done")
 
   private def fixZombieCommits() {
-      val repos = RepoDataDiscovery.discoverRepoDataFromConfig(this).map(Repository.buildUsing)
-
-      repos.foreach { repo =>
-        val repoCommits = repo.loadCommitsSince(Map(), Int.MaxValue)
-
-        repoCommits.commits.foreach { commitsForBranch =>
-          commitsForBranch.commits.foreach { repoCommit =>
-            commitInfoDao.findBySha(repo.repoData.repoName, repoCommit.sha) match {
-              case Some(dbCommit) => fixCommitDates(repoCommit, dbCommit)
-            }
-          }
-        }
+    for {
+      repo <- RepoDataDiscovery.discoverRepoDataFromConfig(this).map(Repository.buildUsing)
+      commitsForBranch <- repo.loadCommitsSince(Map(), Int.MaxValue).commits
+      repoCommit <- commitsForBranch.commits
+    } {
+      commitInfoDao.findBySha(repo.repoName, repoCommit.sha) match {
+        case Some(dbCommit) => fixCommitDates(repoCommit, dbCommit)
       }
     }
+  }
 
-    private def fixCommitDates(repoCommit: CommitInfo, dbCommit: CommitInfo) {
-      def dateCheck(label: String, date: CommitInfo => DateTime) = {
-        val result = date(dbCommit).isEqual(date(repoCommit))
-        val message = s"$label date is invalid (current: ${date(dbCommit)}, actual: ${date(repoCommit).withZone(DateTimeZone.UTC)})"
-        (result, message)
-      }
-
-      val commitDateValid = rule("commitDate") { dateCheck("Commit", { _.commitDate }) }
-      val authorDateValid = rule("authorDate") { dateCheck("Author", { _.authorDate }) }
-      val result = validate(commitDateValid, authorDateValid)
-
-      if (result.errors.nonEmpty) {
-        val fixedCommitInfo = dbCommit.copy(commitDate = repoCommit.commitDate, authorDate = repoCommit.authorDate)
-        logger.debug("Validation failed for commit {} (SHA: {})", fixedCommitInfo.id, fixedCommitInfo.sha)
-        result.errors.foreach { error => logger.debug(error._2.mkString) }
-        logger.debug("Updating commit {}", fixedCommitInfo.id)
-        commitInfoDao.updateCommitAndAuthorDates(fixedCommitInfo)
-      }
+  private def fixCommitDates(repoCommit: CommitInfo, dbCommit: CommitInfo) {
+    def dateCheck(label: String, date: CommitInfo => DateTime) = {
+      val result = date(dbCommit).isEqual(date(repoCommit))
+      val message = s"$label date is invalid (current: ${date(dbCommit)}, actual: ${date(repoCommit).withZone(DateTimeZone.UTC)})"
+      (result, message)
     }
+
+    val commitDateValid = rule("commitDate") { dateCheck("Commit", { _.commitDate }) }
+    val authorDateValid = rule("authorDate") { dateCheck("Author", { _.authorDate }) }
+    val result = validate(commitDateValid, authorDateValid)
+
+    if (result.errors.nonEmpty) {
+      val fixedCommitInfo = dbCommit.copy(commitDate = repoCommit.commitDate, authorDate = repoCommit.authorDate)
+      logger.debug("Validation failed for commit {} (SHA: {})", fixedCommitInfo.id, fixedCommitInfo.sha)
+      result.errors.map(_._2.mkString).foreach { logger.debug(_) }
+      logger.debug("Updating commit {}", fixedCommitInfo.id)
+      commitInfoDao.updateCommitAndAuthorDates(fixedCommitInfo)
+    }
+  }
 }
