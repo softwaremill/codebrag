a5841a34d25eaee87e78d6d156e7ab5b606eeec9
Lukasz Lenart <lukasz.lenart@gmail.com>
2014-12-05 14:47:40 +0100

    Wires hook processor with event bus

--- codebrag-rest/src/main/scala/com/softwaremill/codebrag/EventingConfiguration.scala
+++ codebrag-rest/src/main/scala/com/softwaremill/codebrag/EventingConfiguration.scala
@@ -1,13 +1,13 @@
 package com.softwaremill.codebrag
 
 import akka.actor.Props
-import com.softwaremill.codebrag.common.{Event, StatisticEvent}
+import com.softwaremill.codebrag.common.{Hookable, Event, StatisticEvent}
 import com.softwaremill.codebrag.service.actors.ActorSystemSupport
 import com.softwaremill.codebrag.dao.events.EventDAO
 import com.softwaremill.codebrag.service.followups.FollowupsGenerator
 import com.softwaremill.codebrag.domain.reactions.LikeEvent
 import com.softwaremill.codebrag.domain.reactions.UnlikeEvent
-import com.softwaremill.codebrag.eventstream.StatisticEventsCollector
+import com.softwaremill.codebrag.eventstream.{EventHookPropagator, StatisticEventsCollector}
 import com.softwaremill.codebrag.service.events.EventLogger
 import com.softwaremill.codebrag.dao.user.UserDAO
 import com.softwaremill.codebrag.dao.commitinfo.CommitInfoDAO
@@ -20,15 +20,19 @@ trait EventingConfiguration extends ActorSystemSupport {
   def followupDao: FollowupDAO
   def followupWithReactionsDao: FollowupWithReactionsDAO
   def eventDao: EventDAO
+  def config: AllConfig
 
   lazy val eventLogger = actorSystem.actorOf(Props(classOf[EventLogger]))
   lazy val followupGeneratorActor = actorSystem.actorOf(Props(new FollowupsGenerator(followupDao, userDao, commitInfoDao, followupWithReactionsDao: FollowupWithReactionsDAO)))
   lazy val statsEventsCollector = actorSystem.actorOf(Props(new StatisticEventsCollector(eventDao)))
+  lazy val eventHookPropagator = actorSystem.actorOf(Props(new EventHookPropagator(config.eventHooks)))
 
   def setupEvents() {
     actorSystem.eventStream.subscribe(eventLogger, classOf[Event])
     actorSystem.eventStream.subscribe(followupGeneratorActor, classOf[LikeEvent])
     actorSystem.eventStream.subscribe(followupGeneratorActor, classOf[UnlikeEvent])
     actorSystem.eventStream.subscribe(statsEventsCollector, classOf[StatisticEvent])
+    actorSystem.eventStream.subscribe(eventHookPropagator, classOf[Hookable])
   }
+
 }
