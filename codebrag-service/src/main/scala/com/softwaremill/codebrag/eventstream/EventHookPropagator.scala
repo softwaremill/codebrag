a8e31c51e60d9d2400480427eeef1d41aa55b137
Lukasz Lenart <lukasz.lenart@gmail.com>
2014-12-06 15:04:30 +0100

    Extends with proper JSON serialization

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/eventstream/EventHookPropagator.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/eventstream/EventHookPropagator.scala
@@ -3,12 +3,29 @@ package com.softwaremill.codebrag.eventstream
 import akka.actor.Actor
 import com.ning.http.client.StringPart
 import com.softwaremill.codebrag.common.Hookable
+import com.softwaremill.codebrag.domain.reactions.{LikeEvent, UnlikeEvent}
 import com.typesafe.scalalogging.slf4j.Logging
 import dispatch._
+import org.joda.time.{DateTime, DateTimeZone}
+import org.json4s.FieldSerializer._
+import org.json4s.ext.JodaTimeSerializers
+import org.json4s.jackson.Serialization.{write => jsonWrite}
+import org.json4s.mongo._
+import org.json4s.{DefaultFormats, FieldSerializer}
+
+case class HookEvent(event: Hookable, hookDate: DateTime = DateTime.now(DateTimeZone.UTC))
 
 class EventHookPropagator(hookListeners: Map[String, List[String]]) extends Actor with Logging {
 
-  def toJSON(hookable: Hookable): String = s"{}"
+  val LikeEventSerializer = FieldSerializer[LikeEvent](ignore("clock"))
+  val UnlikeEventSerializer = FieldSerializer[UnlikeEvent](ignore("clock"))
+
+  implicit val formats =
+    DefaultFormats +
+    new ObjectIdSerializer() +
+    LikeEventSerializer +
+    UnlikeEventSerializer ++
+    JodaTimeSerializers.all
 
   def receive = {
     case (event: Hookable) =>
@@ -17,11 +34,18 @@ class EventHookPropagator(hookListeners: Map[String, List[String]]) extends Acto
       for (hookUrl <- hookListeners(event.hookName)) {
         logger.debug(s"Sending ${event.hookName} to $hookUrl")
 
-        val body = new StringPart(event.hookName, toJSON(event))
-        val hook = url(hookUrl).POST.addBodyPart(body).setHeader("Content-Type", "application/json")
-        val response = Http(hook OK as.String)
-        logger.debug(s"Got response: $response")
+        val json = toJSON(HookEvent(event))
+        val body = new StringPart(event.hookName, json)
+        val request = url(hookUrl).POST.addBodyPart(body).setHeader("Content-Type", "application/json")
+
+        Http(request OK as.String).onComplete( (status) =>
+          logger.debug(s"Got response: $status")
+        )
       }
+  }
+
+  private def toJSON(hookEvent: HookEvent) = {
+    jsonWrite(hookEvent)
   }
 
 }
