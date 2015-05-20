f62dc56ef949eaebf61274a3e8723b4c03aea415
Jan Kowalski <jan.kowalski@softwaremill.com>
2015-05-20 08:22:07 +0200

    sbt-idea plugin

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -6,7 +6,8 @@ object Plugins extends Build {
     file("."),
     settings = Defaults.coreDefaultSettings ++ Seq(
       addSbtPlugin("com.earldouglas" %% "xsbt-web-plugin" % "1.0.0-M7"),
-      addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4"),
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0"),      
+addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.7.4"),
       addSbtPlugin("com.typesafe.sbt" % "sbt-scalariform" % "1.1.0"),
       addSbtPlugin("com.gu" % "sbt-teamcity-test-reporting-plugin" % "1.5"),
       addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0"),
