347dc35dc2f0399bfb21daa354bf54d0f133a67f
Paweł Stawicki <pawelstawicki@gmail.com>
2015-07-23 13:56:45 +0200

    user_tokens table primary key is now only single column - "token"

--- codebrag-dao/src/main/resources/db/migration/V18__add_user_tokens.sql
+++ codebrag-dao/src/main/resources/db/migration/V18__add_user_tokens.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS "user_tokens"(
     "user_id" VARCHAR NOT NULL,
     "token" VARCHAR NOT NULL
 );
-ALTER TABLE "user_tokens" ADD CONSTRAINT IF NOT EXISTS "pk" PRIMARY KEY("token", "user_id");
+ALTER TABLE "user_tokens" ADD CONSTRAINT IF NOT EXISTS "pk" PRIMARY KEY("token");
 
 ALTER TABLE "users" DROP COLUMN IF EXISTS "token";
