bef7cb58d9b28b2454d941fe50bab5777f73964f
Lukasz Lenart <lukasz.lenart@gmail.com>
2015-09-09 12:51:53 +0200

    Adds test case to cover publishing the event Refs #48

--- codebrag-service/src/test/scala/com/softwaremill/codebrag/service/commits/CommitImportServiceSpec.scala
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/service/commits/CommitImportServiceSpec.scala
@@ -1,9 +1,12 @@
 package com.softwaremill.codebrag.service.commits
 
-import org.scalatest.{FlatSpec, BeforeAndAfter}
+import com.softwaremill.codebrag.common.ClockSpec
+import com.softwaremill.codebrag.service.events.MockEventBus
+import org.eclipse.jgit.lib.ObjectId
+import org.scalatest.{BeforeAndAfterEach, FlatSpec, BeforeAndAfter}
 import org.scalatest.mock.MockitoSugar
 import org.scalatest.matchers.ShouldMatchers
-import com.softwaremill.codebrag.domain.{RepositoryStatus, MultibranchLoadCommitsResult}
+import com.softwaremill.codebrag.domain.{CommitInfo, CommitsForBranch, RepositoryStatus, MultibranchLoadCommitsResult}
 import org.mockito.Mockito._
 import com.softwaremill.codebrag.dao.repositorystatus.RepositoryStatusDAO
 import com.softwaremill.codebrag.repository.Repository
@@ -12,7 +15,7 @@ import com.softwaremill.codebrag.dao.branchsnapshot.BranchStateDAO
 import com.softwaremill.codebrag.cache.RepositoriesCache
 import com.softwaremill.codebrag.service.config.CommitCacheConfig
 
-class CommitImportServiceSpec extends FlatSpec with MockitoSugar with BeforeAndAfter with ShouldMatchers {
+class CommitImportServiceSpec extends FlatSpec with MockitoSugar with BeforeAndAfter with BeforeAndAfterEach with ShouldMatchers with ClockSpec with MockEventBus {
 
   var repoStatusDao: RepositoryStatusDAO = _
   var branchStateDao: BranchStateDAO = _
@@ -24,7 +27,16 @@ class CommitImportServiceSpec extends FlatSpec with MockitoSugar with BeforeAndA
   val SavedRepoState = Map.empty[String, String]
   val MaxCommitsForNewBranch = 100
   val RepoName = "test-repo"
+
   val LoadedCommits = MultibranchLoadCommitsResult(RepoName, List.empty)
+
+  val commitInfo = CommitInfo(RepoName, "1234", "message", "author", "author@sml.com", "committer", "committer@sml.com", clock.nowUtc, clock.nowUtc, List())
+
+  val FullyLoadedCommits = MultibranchLoadCommitsResult(RepoName, List(CommitsForBranch("branch", List(commitInfo), "1234")))
+
+  override def beforeEach(): Unit = {
+    eventBus.clear()
+  }
 
   before {
     repoStatusDao = mock[RepositoryStatusDAO]
@@ -33,7 +45,7 @@ class CommitImportServiceSpec extends FlatSpec with MockitoSugar with BeforeAndA
     repository = mock[Repository]
     config = mock[CommitCacheConfig]
     when(config.maxCommitsCachedPerBranch).thenReturn(MaxCommitsForNewBranch)
-    service = new CommitImportService(repoStatusDao, branchStateDao, reposCache, config)
+    service = new CommitImportService(repoStatusDao, branchStateDao, reposCache, config, eventBus)
   }
 
   it should "pull changes and load commits from given repo since last saved state" in {
@@ -71,6 +83,21 @@ class CommitImportServiceSpec extends FlatSpec with MockitoSugar with BeforeAndA
     // then
     val expectedRepoStatus = RepositoryStatus.notReady(repository.repoName, Some("oops"))
     verify(repoStatusDao).updateRepoStatus(expectedRepoStatus)
+  }
+
+  it should "emmit event on commits get loaded" in {
+    // given
+    when(branchStateDao.loadBranchesStateAsMap(repository.repoName)).thenReturn(SavedRepoState)
+    when(repository.loadCommitsSince(SavedRepoState, config.maxCommitsCachedPerBranch)).thenReturn(FullyLoadedCommits)
+    when(repository.currentHead).thenReturn(ObjectId.zeroId())
+
+    // when
+    service.importRepoCommits(repository)
+
+    // then
+    verify(repository).pullChanges()
+    verify(repository).loadCommitsSince(SavedRepoState, config.maxCommitsCachedPerBranch)
+    getEvents.size should be(1)
   }
 
 }
