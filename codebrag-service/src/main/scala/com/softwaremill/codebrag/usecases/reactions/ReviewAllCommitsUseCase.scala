2827598fab5260ecb54f966c57f3acacbbf9e0bd
Lukasz Lenart <lukasz.lenart@gmail.com>
2016-02-24 10:15:42 +0100

    Rolls back to userId instead of User object

--- codebrag-service/src/main/scala/com/softwaremill/codebrag/usecases/reactions/ReviewAllCommitsUseCase.scala
+++ codebrag-service/src/main/scala/com/softwaremill/codebrag/usecases/reactions/ReviewAllCommitsUseCase.scala
@@ -3,15 +3,18 @@ package com.softwaremill.codebrag.usecases.reactions
 import com.softwaremill.codebrag.cache.{RepositoriesCache, UserReviewedCommitsCache}
 import com.softwaremill.codebrag.common.{EventBus, Clock}
 import com.softwaremill.codebrag.dao.commitinfo.CommitInfoDAO
-import com.softwaremill.codebrag.domain.{ReviewedCommit, User}
+import com.softwaremill.codebrag.dao.user.UserDAO
+import com.softwaremill.codebrag.domain.ReviewedCommit
 import com.softwaremill.codebrag.domain.reactions.AllCommitsReviewedEvent
 import com.softwaremill.codebrag.finders.commits.toreview.ToReviewBranchCommitsFilter
 import com.typesafe.scalalogging.slf4j.Logging
+import org.bson.types.ObjectId
 
 /**
  * Handles user activity when user wants to mark all commits as reviewed
  */
 class ReviewAllCommitsUseCase(
+  userDao: UserDAO,
   eventBus: EventBus,
   commitInfoDAO: CommitInfoDAO,
   repoCache: RepositoriesCache,
@@ -22,18 +25,30 @@ class ReviewAllCommitsUseCase(
 
   type ReviewAllCommitsResult = Either[String, Unit]
 
-  def execute(repoName: String, branchName: String, user: User): ReviewAllCommitsResult = {
+  def execute(repoName: String, branchName: String, userId: ObjectId): ReviewAllCommitsResult = {
 
-    val allBranchCommits = repoCache.getBranchCommits(repoName, branchName)
-    val toReviewBranchCommits = toReviewCommitsFilter.filterCommitsToReview(allBranchCommits, user, repoName)
+    val result = for {
+      user <- userDao.findById(userId)
+    } yield {
+      logger.debug(s"Using $user to find all commits to review")
 
-    commitInfoDAO.findByShaList(repoName, toReviewBranchCommits).foreach { commit =>
-      val reviewedCommit = ReviewedCommit(commit.sha, user.id, repoName, clock.nowUtc)
-      reviewedCommitsCache.markCommitAsReviewed(reviewedCommit)
+      val allBranchCommits = repoCache.getBranchCommits(repoName, branchName)
+      val toReviewBranchCommits = toReviewCommitsFilter.filterCommitsToReview(allBranchCommits, user, repoName)
+
+      logger.debug(s"Found ${toReviewBranchCommits.size} commits to mark as reviewed")
+
+      commitInfoDAO.findByShaList(repoName, toReviewBranchCommits).foreach { commit =>
+        val reviewedCommit = ReviewedCommit(commit.sha, user.id, repoName, clock.nowUtc)
+
+        logger.info(s"Marking commit ${reviewedCommit.sha} as reviewed by user $user")
+        reviewedCommitsCache.markCommitAsReviewed(reviewedCommit)
+      }
+
+      eventBus.publish(AllCommitsReviewedEvent(repoName, branchName, userId))
     }
 
-    Right {
-      eventBus.publish(AllCommitsReviewedEvent(repoName, branchName, user.id))
+    result.toRight {
+      s"Cannot mark all commits in branch $branchName as reviewed by user $userId"
     }
   }
 
--- codebrag-service/src/test/scala/com/softwaremill/codebrag/usecases/reactions/ReviewAllCommitsUseCaseSpec.scala
+++ codebrag-service/src/test/scala/com/softwaremill/codebrag/usecases/reactions/ReviewAllCommitsUseCaseSpec.scala
@@ -3,6 +3,7 @@ package com.softwaremill.codebrag.usecases.reactions
 import com.softwaremill.codebrag.cache.{BranchCommitCacheEntry, RepositoriesCache, UserReviewedCommitsCache}
 import com.softwaremill.codebrag.common.{ClockSpec, EventBus}
 import com.softwaremill.codebrag.dao.commitinfo.CommitInfoDAO
+import com.softwaremill.codebrag.dao.user.UserDAO
 import com.softwaremill.codebrag.domain.{User, PartialCommitInfo, ReviewedCommit}
 import com.softwaremill.codebrag.domain.builder.CommitInfoAssembler
 import com.softwaremill.codebrag.domain.reactions.AllCommitsReviewedEvent
@@ -17,11 +18,13 @@ class ReviewAllCommitsUseCaseSpec
   extends FlatSpec with MockitoSugar with ShouldMatchers with BeforeAndAfterEach with ClockSpec {
 
   var eventBus: EventBus = _
+  var userDAO: UserDAO = _
   var commitInfoDao: CommitInfoDAO = _
   var repositoriesCache: RepositoriesCache = _
   var toReviewBranchCommitsFilter: ToReviewBranchCommitsFilter = _
   var reviewedCommitsCache: UserReviewedCommitsCache = _
 
+  val userId = ObjectId.get
   var user: User = _
 
   var useCase: ReviewAllCommitsUseCase = _
@@ -34,18 +37,19 @@ class ReviewAllCommitsUseCaseSpec
 
   override def beforeEach() {
     eventBus = mock[EventBus]
+    userDAO = mock[UserDAO]
     commitInfoDao = mock[CommitInfoDAO]
     repositoriesCache = mock[RepositoriesCache]
     toReviewBranchCommitsFilter = mock[ToReviewBranchCommitsFilter]
     reviewedCommitsCache = mock[UserReviewedCommitsCache]
 
-    val userId = ObjectId.get
     user = mock[User]
     when(user.id).thenReturn(userId)
 
-    useCase = new ReviewAllCommitsUseCase(eventBus, commitInfoDao, repositoriesCache, toReviewBranchCommitsFilter, reviewedCommitsCache)
+    useCase = new ReviewAllCommitsUseCase(userDAO, eventBus, commitInfoDao, repositoriesCache, toReviewBranchCommitsFilter, reviewedCommitsCache)
 
     // given
+    when(userDAO.findById(userId)).thenReturn(Option(user))
     when(repositoriesCache.getBranchCommits(RepoName, BranchName)).thenReturn(List(commitCache))
     when(toReviewBranchCommitsFilter.filterCommitsToReview(List(commitCache), user, RepoName)).thenReturn(List(commit.sha))
     when(commitInfoDao.findByShaList(RepoName, List(commit.sha))).thenReturn(List(PartialCommitInfo(commit)))
@@ -53,7 +57,7 @@ class ReviewAllCommitsUseCaseSpec
 
   it should "generate all commits reviewed event" in {
     // when
-    useCase.execute(RepoName, BranchName, user)
+    useCase.execute(RepoName, BranchName, userId)
 
     // then
     verify(eventBus).publish(AllCommitsReviewedEvent(RepoName, BranchName, user.id))
@@ -61,11 +65,10 @@ class ReviewAllCommitsUseCaseSpec
 
   it should "mark all commits as reviewed if commits were found" in {
     // when
-    useCase.execute(RepoName, BranchName, user)
+    useCase.execute(RepoName, BranchName, userId)
 
     // then
     val expectedCommitReviewed = ReviewedCommit(commit.sha, user.id, RepoName, clock.nowUtc)
     verify(reviewedCommitsCache).markCommitAsReviewed(expectedCommitReviewed)
   }
-
 }
